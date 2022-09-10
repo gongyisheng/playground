8818de8e8e232b094c3358f8782750e5117b1950
Yisheng Gong <yisheng_gong@onmail.com>
2022-09-10 13:24:26 -0400

    add async queue

---
+++ python/asyncio/queue.py
@@ -0,0 +1,58 @@
+from ast import parse
+import asyncio
+import itertools as it
+import os
+import random
+import time
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--nprod", type=int, default=5)
+    parser.add_argument("-c", "--ncon", type=int, default=10)
+    return parser.parse_args()
+
+async def makeitem(size: int = 5) -> str:
+    return os.urandom(size).hex()
+
+async def randsleep(caller=None) -> None:
+    i = random.randint(0, 10)
+    if caller:
+        print(f"{caller} sleeping for {i} seconds.")
+    await asyncio.sleep(i)
+
+async def produce(name: int, q: asyncio.Queue) -> None:
+    n = random.randint(0, 10)
+    for _ in it.repeat(None, n):  # Synchronous loop for each single producer
+        await randsleep(caller=f"Producer {name}")
+        i = await makeitem()
+        t = time.perf_counter()
+        await q.put((i, t))
+        print(f"Producer {name} added <{i}> to queue.")
+
+async def consume(name: int, q: asyncio.Queue) -> None:
+    while True:
+        await randsleep(caller=f"Consumer {name}")
+        i, t = await q.get()
+        now = time.perf_counter()
+        print(f"Consumer {name} got element <{i}>"
+              f" in {now-t:0.5f} seconds.")
+        q.task_done()
+
+async def main(nprod: int, ncon: int):
+    q = asyncio.Queue()
+    producers = [asyncio.create_task(produce(n, q)) for n in range(nprod)]
+    consumers = [asyncio.create_task(consume(n, q)) for n in range(ncon)]
+    await asyncio.gather(*producers)
+    await q.join()  # Implicitly awaits consumers, too
+    for c in consumers:
+        c.cancel()
+
+if __name__ == "__main__":
+    # usage: python queue.py -p <num of producer> -c <num of consumer>
+    random.seed(444)
+    start = time.perf_counter()
+    ns = parse_args()
+    asyncio.run(main(**ns.__dict__))
+    elapsed = time.perf_counter() - start
+    print(f"Program completed in {elapsed:0.5f} seconds.")
