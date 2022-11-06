9a6f75b91ad675a26938b6a98b77a9f5e39662b7
Yisheng Gong <yisheng_gong@onmail.com>
2022-11-05 22:09:56 -0400

    update gitignore

---
+++ cpp/helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! \n"; // c-out, see-out
+  cout << "Hello World!" << endl; // use endl to print a new line
+  cout << "I am learning C++";
+  return 0;
+}
