a2b960b99e48e7fef5306238cb608f08e2288729
Yisheng Gong <yisheng_gong@onmail.com>
2022-11-05 22:37:54 -0400

    add variable

---
+++ cpp/variable.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // type variableName = value;
+    int myInt = 10;
+    cout << "myInt is: " << myInt << endl;
+    double myDouble = 10.5;
+    cout << "myDouble is: " << myDouble << endl;
+    char myChar = 'a';
+    cout << "myChar is: " << myChar << endl;
+    string myString = "Hello World!";
+    cout << "myString is: " << myString << endl;
+    bool myBool = true;
+    cout << "myBool is: " << myBool << endl;
+
+    int x = 5, y = 6, z = 50;
+    cout << "X+Y+Z = "<< x + y + z << endl;
+    return 0;
+}
