eeb6820b9b53ccf940b4d68bc8cd037b44d4a7b8
Yisheng Gong <yisheng_gong@onmail.com>
2022-11-05 22:44:00 -0400

    add input

---
+++ cpp/input.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x; 
+    cout << "Type a number: "; // Type a number and press enter
+    cin >> x; // Get user input from the keyboard
+    cout << "Your number is: " << x << endl; // Display the input value
+}
