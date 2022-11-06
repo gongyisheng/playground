b367c2f14eaad3905a796418f6621f144de39651
Yisheng Gong <yisheng_gong@onmail.com>
2022-11-05 23:17:53 -0400

    add math

---
+++ cpp/math.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << max(5, 10) << endl; // 10
+    cout << min(5, 10) << endl; // 5
+    cout << sqrt(64) << endl; // 8
+    cout << round(2.6) << endl; // 3
+    cout << log(2) << endl; // 0.693147
+}
