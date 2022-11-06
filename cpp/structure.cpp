d03af614c677b178dff279681e8b46ed769b012b
Yisheng Gong <yisheng_gong@onmail.com>
2022-11-06 01:36:03 -0400

    add struct

---
+++ cpp/structure.cpp
@@ -0,0 +1,18 @@
+#include<string>
+#include<iostream>
+using namespace std;
+
+struct demoStruct{   // Structure declaration, name=demoStruct
+  int myNum;         // Member (int variable)
+  string myString;   // Member (string variable)
+} myStructure, myStructure1, myStructure2;       // Structure variable
+
+demoStruct myStructure4; // Structure variable
+
+int main() {
+    myStructure.myNum = 15;    // Accessing members
+    myStructure.myString = "Some text";
+    cout << myStructure.myNum << endl;
+    cout << myStructure.myString << endl;
+    return 0;
+}
