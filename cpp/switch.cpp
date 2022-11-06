9e07b5c9e134f1ec5367f4a3a0650ec2dcbb41c9
Yisheng Gong <yisheng_gong@onmail.com>
2022-11-06 00:06:35 -0400

    add switch

---
+++ cpp/switch.cpp
@@ -0,0 +1,33 @@
+#include<string>
+#include<iostream>
+using namespace std;
+
+int main() {
+    int day;
+    cout << "input number [1,7]" << endl;
+    cin >> day;
+
+    switch (day) {
+    case 1:
+        cout << "Monday" << endl;
+        break;
+    case 2:
+        cout << "Tuesday" << endl;
+        break;
+    case 3:
+        cout << "Wednesday" << endl;
+        break;
+    case 4:
+        cout << "Thursday" << endl;
+        break;
+    case 5:
+        cout << "Friday" << endl;
+        break;
+    case 6:
+        cout << "Saturday" << endl;
+        break;
+    case 7:
+        cout << "Sunday" << endl;
+        break;
+    }
+}
