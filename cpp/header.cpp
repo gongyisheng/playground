e0a9ee26ef6d7fa5ab7a995d810ace047f40fab3
Yisheng Gong <yisheng_gong@onmail.com>
2022-11-07 00:37:48 -0500

    update header file

--- cpp/header.cpp
+++ cpp/header.cpp
@@ -2,9 +2,15 @@
 #include "header_sum.h"
 using namespace std;
 
+int _sumOfTwoNumbers(int a, int b) {
+    return a + b;
+}
+
 int main() {
     int a = 5;
     int b = 10;
     cout << sumOfTwoNumbers(a, b) << endl;
+    cout << _sumOfTwoNumbers(a, b) << endl;
+    cout << gongyisheng::sumOfTwoNumbers_gys(a, b) << endl; // use function in another namespace
     return 0;
 }
--- cpp/header_sum.h
+++ cpp/header_sum.h
@@ -1,4 +1,19 @@
+// Best practice: 
+// 1. If a header file is paired with a code file (e.g. add.h with add.cpp), 
+// they should both have the same base name (add).
+// 2. Header files should generally not contain function and variable definitions, 
+// so as not to violate the one definition rule. An exception is made for symbolic constants
+
+// define a function in a namespace
+namespace gongyisheng{
+    int sumOfTwoNumbers_gys(int a, int b) {
+        return a + b;
+    }
+}
 
 int sumOfTwoNumbers(int a, int b) {
     return a + b;
 };
+
+// function prototype for add.h -- don't forget the semicolon!
+int _sumOfTwoNumbers(int a, int b);
