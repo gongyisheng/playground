8cc59eb1f7825f995246d8bd475ce6cd949c3fb1
Yisheng Gong <yisheng_gong@onmail.com>
2022-11-09 00:14:06 -0500

    add initialization

--- cpp/variable.cpp
+++ cpp/variable.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main() {
+    // initialize a variable
+    int a1, a2, a3; // Default initialization
+    int a4 = 10;    // Copy initialization
+    int a5 = {10};  // Brace initialization
+
     // type variableName = value;
     int myInt = 10;
     cout << "myInt is: " << myInt << endl;
