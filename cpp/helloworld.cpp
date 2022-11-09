f66aef27707eb422f094bb3545d3089e50fec8d3
Yisheng Gong <yisheng_gong@onmail.com>
2022-11-09 00:22:18 -0500

    add knowledge of endl

--- cpp/helloworld.cpp
+++ cpp/helloworld.cpp
@@ -5,5 +5,11 @@ int main() {
   cout << "Hello World! \n"; // c-out, see-out
   cout << "Hello World!" << endl; // use endl to print a new line
   cout << "I am learning C++";
+  /*
+  endl does two jobs: 
+  1. move the cursor to the next line
+  2. flush the buffer (print out cached output immediately)
+  /n only moves the cursor to the next line
+  */
   return 0;
 }
