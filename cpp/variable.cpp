1353a4573b4f7d68a3f52e3212f8ca5ca54c9f0b
Yisheng Gong <yisheng_gong@onmail.com>
2022-11-09 02:05:09 -0500

    add undefined behavior

--- cpp/variable.cpp
+++ cpp/variable.cpp
@@ -31,4 +31,15 @@ int main() {
     int x = 5, y = 6, z = 50;
     cout << "X+Y+Z = "<< x + y + z << endl;
     return 0;
+    /*
+    Avoid undefined behavior! 
+    Code implementing undefined behavior may exhibit any of the following symptoms:
+    - Your program produces different results every time it is run.
+    - Your program consistently produces the same incorrect result.
+    - Your program behaves inconsistently (sometimes produces the correct result, sometimes not).
+    - Your program seems like its working but produces incorrect results later in the program.
+    - Your program crashes, either immediately or later.
+    - Your program works on some compilers but not others.
+    - Your program works until you change some other seemingly unrelated code.
+    */
 }
