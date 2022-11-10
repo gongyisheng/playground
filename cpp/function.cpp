2ddaed44e9f9ac51824cd6175bca03432ca12172
Yisheng Gong <yisheng_gong@onmail.com>
2022-11-10 01:06:29 -0500

    add forward declaration

--- cpp/function.cpp
+++ cpp/function.cpp
@@ -67,3 +67,21 @@ int main() {
     cout << "Double: " << myNum2 << endl;
     return 0;
 }
+
+// If you define new functions here, it cannot be called. But you can use forward declaration.
+
+// forward declaration
+/*
+int add(int x, int y); // forward declaration of add() (using a function declaration)
+
+int main()
+{
+    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n'; // this works because we forward declared add() above
+    return 0;
+}
+
+int add(int x, int y) // even though the body of add() isn't defined until here
+{
+    return x + y;
+}
+*/
