3cfa0be686fac98526f2964305aad60b25b910ac
Yisheng Gong <yisheng_gong@onmail.com>
2022-11-13 22:36:22 -0500

    add description of templates

--- cpp/template.cpp
+++ cpp/template.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+// Templates in C++ are mostly used for generic programming.
+
 // We start with the kind of generic programming you might be familiar with. 
 // To define a class or function that takes a type parameter:
 template<class T>
