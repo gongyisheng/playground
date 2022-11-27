068a445b1d4cfd662145c17ab7c779a2e52f42aa
Yisheng Gong <yisheng_gong@onmail.com>
2022-11-26 22:23:20 -0500

    update example

--- python/profile/cprofile_exp.py
+++ python/profile/cprofile_exp.py
@@ -20,7 +20,7 @@ def fib_seq(n):
 
 if __name__ == "__main__":
     p = cProfile.Profile()
-    p.run('fib_seq(30)')
+    p.run('fib_seq(30),fib_seq(35)')
     p.dump_stats('output.prof')
 
     stream = open('output.txt', 'w')
@@ -28,4 +28,9 @@ if __name__ == "__main__":
     stats.sort_stats('cumtime')
     s = stats.print_stats()
     for k,v in s.stats.items():
-        print(f"file: {k[0]}, line: {k[1]}, func: {k[2]}, ncalls: {v[0]}, tottime: {v[1]}, cumtime: {v[2]}")
+        cc, nc, tt, ct, callers = v
+        per_call_1 = 0 if nc==0 else float(tt) / nc
+        per_call_2 = 0 if cc==0 else float(ct) / cc
+        # the output will not be sorted because it's from dict
+        print("[cProfile STATS] file: %s, line: %s, func: %s, direct_call: %s, total_call: %s, total_time: %s, tt_per_call: %s, cum_time: %s, ct_per_call, %s" % (k[0], k[1], k[2], str(cc), str(nc), str(tt), str(per_call_1), str(ct), str(per_call_2)))
+    stream.close()
