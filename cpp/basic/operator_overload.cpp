c1a9e67bb55785c189d7cd87897b616885e64965
Yisheng Gong <yisheng_gong@onmail.com>
2022-12-04 17:12:32 -0500

    add operator overload

---
+++ cpp/basic/operator_overload.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+ 
+class Box
+{
+   public:
+ 
+      double getVolume(void)
+      {
+         return length * breadth * height;
+      }
+      void setLength( double len )
+      {
+          length = len;
+      }
+ 
+      void setBreadth( double bre )
+      {
+          breadth = bre;
+      }
+ 
+      void setHeight( double hei )
+      {
+          height = hei;
+      }
+      // operator overload
+      Box operator+(const Box& b)
+      {
+         Box box;
+         box.length = this->length + b.length;
+         box.breadth = this->breadth + b.breadth;
+         box.height = this->height + b.height;
+         return box;
+      }
+   private:
+      double length;
+      double breadth;
+      double height;
+};
+
+int main()
+{
+   Box Box1;
+   Box Box2;
+   Box Box3;     
+   double volume = 0.0;
+ 
+   // Box1
+   Box1.setLength(6.0); 
+   Box1.setBreadth(7.0); 
+   Box1.setHeight(5.0);
+ 
+   // Box2
+   Box2.setLength(12.0); 
+   Box2.setBreadth(13.0); 
+   Box2.setHeight(10.0);
+ 
+   // Box1
+   volume = Box1.getVolume();
+   cout << "Volume of Box1 : " << volume <<endl;
+ 
+   // Box2
+   volume = Box2.getVolume();
+   cout << "Volume of Box2 : " << volume <<endl;
+ 
+   // Box3 = Box1+Box2
+   Box3 = Box1 + Box2;
+ 
+   // Box3
+   volume = Box3.getVolume();
+   cout << "Volume of Box3 : " << volume <<endl;
+ 
+   return 0;
+}
