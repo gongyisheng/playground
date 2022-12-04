bf19c0bb6b91d618af39c2d9354d8885cf6f9e7a
Yisheng Gong <yisheng_gong@onmail.com>
2022-12-04 18:01:33 -0500

    add rvalue reference but to be fixed

---
+++ cpp/basic/reference_rvalue.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+class foo{
+    public:
+        int a;
+        int b;
+        foo(){
+            this->a = 0;
+            this->b = 0;
+        }
+        foo(int a, int b){
+            this->a = a;
+            this->b = b;
+        }
+        // copy, use lvalue reference
+        foo(const foo& other){
+            this->a = other.a;
+            this->b = other.b;
+        }
+        // move, use rvalue reference
+        foo(foo&& other){
+            this->a = other.a;
+            this->b = other.b;
+            other.a = 0;
+            other.b = 0;
+        }
+}
+
+// to be fixed
+int main(){
+    foo f1 = foo(1,2);
+    foo f2 = foo(foo(1,2)); // copy
+    foo f3((foo())); // move
+    foo f4 = std::move(f3); // move
+    foo&& f5 = foo(); // move
+    std::cout << f1.a << " " << f1.b << std::endl;
+    std::cout << f2.a << " " << f2.b << std::endl;
+    return 0;
+}
