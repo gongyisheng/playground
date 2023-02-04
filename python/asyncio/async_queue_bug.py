b3f66951c1e45d92260b22c8245ed77741f54a4e
Yisheng Gong <yisheng_gong@onmail.com>
2023-02-03 19:41:08 -0500

    update

--- python/asyncio/async_queue_bug.py
+++ python/asyncio/async_queue_bug.py
@@ -8,8 +8,7 @@ async def get():
     while True:
         n = await q.get()
         print(f"get {n}")
-        yield n
-        print(f"yield {n}")
+        return n
 
 
 async def put(n):
@@ -18,8 +17,8 @@ async def put(n):
 
 
 async def listen():
-    async for i in get():
-        print(i)
+    while True:
+        await get()
 
 
 async def write():
@@ -31,4 +30,12 @@ async def write():
     await asyncio.sleep(1)
     await put(3)
 
-asyncio.run(asyncio.wait([listen(), write()]))
+async def main():
+    # asyncio.wait([write(), listen()])
+    await asyncio.wait_for(asyncio.gather(write(), listen()), timeout=30)
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
+    # asyncio.run(main())
