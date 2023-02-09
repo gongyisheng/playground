66461bb5c7e976bbc30517524d415b9e305f256a
Yisheng Gong <yisheng_gong@onmail.com>
2023-02-08 19:34:42 -0500

    add comments

--- python/asyncio/async_queue_bug.py
+++ python/asyncio/async_queue_bug.py
@@ -35,7 +35,9 @@ async def main():
     await asyncio.wait_for(asyncio.gather(write(), listen()), timeout=30)
 
 if __name__ == "__main__":
+    # following code works in python 3.8.10
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
     loop.close()
+    # following code doesn't work in python 3.8.10, but works in 3.10.0
     # asyncio.run(main())
