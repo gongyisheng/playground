badbcc08b2813c1c2e283e86f6d097a052045226
Yisheng Gong <yisheng_gong@onmail.com>
2023-03-25 00:46:27 -0400

    add async wrapper for runtime

--- python/wrapper/track_usage.py
+++ python/wrapper/track_usage.py
@@ -1,3 +1,4 @@
+import asyncio
 import time
 
 usage_data = {}
@@ -21,13 +22,31 @@ def track_time(metric):
             end = time.perf_counter()
             usage_data[_metric] = usage_data.get(_metric, 0) + (end - start)*1000
             print(f"record time: _metric={_metric}, data={usage_data[_metric]}")
-        return wrapper
+
+        async def async_wrapper(*args, **kwargs):
+            _metric = metric + "_time"
+            start = time.perf_counter()
+            await func(*args, **kwargs)
+            end = time.perf_counter()
+            usage_data[_metric] = usage_data.get(_metric, 0) + (end - start)*1000
+            print(f"record time: _metric={_metric}, data={usage_data[_metric]}")
+        
+        if asyncio.iscoroutinefunction(func):
+            return async_wrapper
+        else:
+            return wrapper
     return decorator
 
 @track_count("IO")
 @track_time("IO")
 def IO(wait=1):
     time.sleep(wait)
+    pass
+
+@track_count("IO")
+@track_time("IO")
+async def IO(wait=1):
+    await asyncio.sleep(wait)
     pass
 
 def display_usage():
@@ -37,6 +56,13 @@ def main():
     for i in range(5):
         IO(i)
 
-if __name__ == '__main__':
-    main()
+async def async_main():
+    for i in range(5):
+        await IO(i)
+
+async def async_test():
+    await async_main()
     display_usage()
+
+if __name__ == '__main__':
+    asyncio.run(async_test())
