2edb4550f73a5586761dd8ddbe280c0a820db6c5
Yisheng Gong <yisheng_gong@onmail.com>
2023-03-24 23:25:50 -0400

    improve display

--- openai/chat.py
+++ openai/chat.py
@@ -5,12 +5,12 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def chat():
     for i in range(50):
-        prompt = input("You: ")
+        prompt = input("You:\n")
         completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": prompt}]
         )
-        print(f"ChatGPT: {completion.choices[0].message}")
+        print(f"ChatGPT:\n{str(completion.choices[0].message['content'])}")
 
 if __name__ == "__main__":
     chat()
