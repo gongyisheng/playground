c70a98506abb6b11db3be67e4e958e20d8952246
Yisheng Gong <yisheng_gong@onmail.com>
2023-04-03 00:08:42 -0400

    add explain code

--- openai/prompt.py
+++ openai/prompt.py
@@ -1,5 +1,6 @@
 prompt = {
     "translator": "I want you to act as an English translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in English. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. My sentence is %s",
     "travel_agent": "I want you to act as a travel guide. I will write you my location and you will suggest a place to visit near my location. In some cases, I will also give you the type of places I will visit. You will also suggest me places of similar type that are close to my first location. My first suggestion request is %s",
-    "explain": "Explain detailed with examples: %s",
+    "explain_general": "Explain detailed with examples: %s",
+    "explain_code": "You are a programmer. I will give you a code snippet and you will explain it in detail with examples. My code snippet is %s" 
 }
