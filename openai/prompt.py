9d5c9bbd074e4f7dfad78e75bd5a681f4e1336a6
Yisheng Gong <yisheng_gong@onmail.com>
2023-04-02 23:59:45 -0400

    add explain

--- openai/prompt.py
+++ openai/prompt.py
@@ -1,4 +1,5 @@
 prompt = {
     "translator": "I want you to act as an English translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in English. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. My sentence is %s",
-    "travel_agent": "I want you to act as a travel guide. I will write you my location and you will suggest a place to visit near my location. In some cases, I will also give you the type of places I will visit. You will also suggest me places of similar type that are close to my first location. My first suggestion request is %s"
+    "travel_agent": "I want you to act as a travel guide. I will write you my location and you will suggest a place to visit near my location. In some cases, I will also give you the type of places I will visit. You will also suggest me places of similar type that are close to my first location. My first suggestion request is %s",
+    "explain": "Explain detailed with examples: %s",
 }
