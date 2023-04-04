77bb2ac6adb5728ab4ed43ae73b7a577fff189e2
Yisheng Gong <yisheng_gong@onmail.com>
2023-04-04 15:25:23 -0400

    add async generator comb

---
+++ python/asyncio/async_generator_comb.py
@@ -0,0 +1,20 @@
+import asyncio
+
+async def generator(limit=100):
+    for i in range(limit):
+        await asyncio.sleep(0.1)
+        yield i
+
+async def IO(id):
+    await asyncio.sleep(1)
+    print(f"id={id}")
+
+async def main():
+    iter = generator()
+    async for i in iter:
+        await IO(i)
+        await IO(str(i)+"_")
+    print("Done")
+
+if __name__ == '__main__':
+    asyncio.run(main())
