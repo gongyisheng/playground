fbe723f82f6d9afdd3ca4fb47e8b04ce0ff81927
Yisheng Gong <yisheng_gong@onmail.com>
2023-04-15 19:37:43 -0400

    add inf loop experiment

--- network/experiment/inf_loop.py
+++ network/experiment/inf_loop.py
@@ -1,8 +1,14 @@
-
+import random
 def run_inf_loop():
     a = 0
     while True:
-        a = a/2
+        rand = random.randint(0, 1000)
+        if rand % 2 == 0:
+            a += rand
+        else:
+            a -= rand
+        if a > 1_000_000_000:
+            a = 0
 
 if __name__ == '__main__':
     run_inf_loop()
