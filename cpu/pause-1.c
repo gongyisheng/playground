f740a337c0ea3b948844ea6cac3c313c4d212fbb
Yisheng Gong <yisheng_gong@onmail.com>
2023-04-17 14:10:25 -0400

    add pause-1

---
+++ cpu/pause-1.c
@@ -0,0 +1,10 @@
+int main() {
+
+	while(1) {
+        // "asm" keyword
+        // it indicates to the compiler that the following code 
+        // will be written in assembly language instead of C
+        __asm__ ("pause");
+	}
+    return 0;
+}
