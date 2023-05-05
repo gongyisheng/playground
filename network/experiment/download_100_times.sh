49d4dcdfa1d1d98186052d2004b60e194cab0957
Yisheng Gong <yisheng_gong@onmail.com>
2023-05-05 10:08:26 -0700

    update ip

--- network/experiment/download_100_times.sh
+++ network/experiment/download_100_times.sh
@@ -1,5 +1,5 @@
 for i in {1..100}
 do 
     echo "Downloading $i"
-    curl http://172.31.82.1:800 0/0000000000000000.data --output ~/0000000000000000.data
+    curl 172.31.92.214:8000/0000000000000000.data --output ~/0000000000000000.data
 done
