f2eaa4f34d08f9a637f61c57abdf6c640ba7ab12
Yisheng Gong <yisheng_gong@onmail.com>
2023-05-16 21:28:39 -0700

    add row test

--- mysql/create_table.sql
+++ mysql/create_table.sql
@@ -9,3 +9,102 @@ CREATE TABLE 1mb_test (
     payload VARCHAR(1000000),
     PRIMARY KEY (id)
 );
+
+CREATE TABLE 100k_row_test(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`person_id` int NOT NULL,
+	`person_name` VARCHAR(200),
+	`insert_time` int,
+	`update_time` int,
+    PRIMARY KEY (`id`),
+    KEY `query_by_update_time` (`update_time`),
+    KEY `query_by_person_id_insert_time` (`insert_time`)
+);
+
+CREATE TABLE 200k_row_test(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`person_id` int NOT NULL,
+	`person_name` VARCHAR(200),
+	`insert_time` int,
+	`update_time` int,
+    PRIMARY KEY (`id`),
+    KEY `query_by_update_time` (`update_time`),
+    KEY `query_by_person_id_insert_time` (`insert_time`)
+);
+
+CREATE TABLE 500k_row_test(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`person_id` int NOT NULL,
+	`person_name` VARCHAR(200),
+	`insert_time` int,
+	`update_time` int,
+    PRIMARY KEY (`id`),
+    KEY `query_by_update_time` (`update_time`),
+    KEY `query_by_person_id_insert_time` (`insert_time`)
+);
+
+CREATE TABLE 1m_row_test(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`person_id` int NOT NULL,
+	`person_name` VARCHAR(200),
+	`insert_time` int,
+	`update_time` int,
+    PRIMARY KEY (`id`),
+    KEY `query_by_update_time` (`update_time`),
+    KEY `query_by_person_id_insert_time` (`insert_time`)
+);
+
+CREATE TABLE 2m_row_test(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`person_id` int NOT NULL,
+	`person_name` VARCHAR(200),
+	`insert_time` int,
+	`update_time` int,
+    PRIMARY KEY (`id`),
+    KEY `query_by_update_time` (`update_time`),
+    KEY `query_by_person_id_insert_time` (`insert_time`)
+);
+
+CREATE TABLE 5m_row_test(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`person_id` int NOT NULL,
+	`person_name` VARCHAR(200),
+	`insert_time` int,
+	`update_time` int,
+    PRIMARY KEY (`id`),
+    KEY `query_by_update_time` (`update_time`),
+    KEY `query_by_person_id_insert_time` (`insert_time`)
+);
+
+CREATE TABLE 10m_row_test(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`person_id` int NOT NULL,
+	`person_name` VARCHAR(200),
+	`insert_time` int,
+	`update_time` int,
+    PRIMARY KEY (`id`),
+    KEY `query_by_update_time` (`update_time`),
+    KEY `query_by_person_id_insert_time` (`insert_time`)
+);
+
+CREATE TABLE 20m_row_test(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`person_id` int NOT NULL,
+	`person_name` VARCHAR(200),
+	`insert_time` int,
+	`update_time` int,
+    PRIMARY KEY (`id`),
+    KEY `query_by_update_time` (`update_time`),
+    KEY `query_by_person_id_insert_time` (`insert_time`)
+);
+
+CREATE TABLE 20m_row_test(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`person_id` int NOT NULL,
+	`person_name` VARCHAR(200),
+	`insert_time` int,
+	`update_time` int,
+    PRIMARY KEY (`id`),
+    KEY `query_by_update_time` (`update_time`),
+    KEY `query_by_person_id_insert_time` (`insert_time`)
+);
