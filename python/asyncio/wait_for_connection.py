ca564d20e508d72bbe20a98b588a10e80445b76b
Yisheng Gong <yisheng_gong@onmail.com>
2023-06-15 15:00:41 -0700

    add wait for connection script

---
+++ python/asyncio/wait_for_connection.py
@@ -0,0 +1,22 @@
+import asyncio
+import time
+
+pool = asyncio.Queue(5)
+
+async def worker(i):
+    while True:
+        obj = await pool.get()
+        print(f'{time.time()} worker {i} start')
+        await asyncio.sleep(1)
+        await pool.put(obj)
+        print(f'{time.time()} worker {i} end')
+
+async def main():
+    for i in range(5):
+        await pool.put(object())
+    tasks = [asyncio.create_task(worker(i)) for i in range(20)]
+    await asyncio.gather(*tasks)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
