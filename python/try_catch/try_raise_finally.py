c2539156676c0d6d9e37ca4c04db2baecaa31c3f
Yisheng Gong <yisheng_gong@onmail.com>
2023-06-19 02:20:36 -0700

    add try raise finally case

---
+++ python/try_catch/try_raise_finally.py
@@ -0,0 +1,11 @@
+def main():
+    try:
+        raise Exception("Exception from try")
+    except Exception as e:
+        raise e
+    finally:
+        print("finally")
+        # return 3 # if uncommented, 3 will be returned
+
+if __name__ == "__main__":
+    main()
