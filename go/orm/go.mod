1ce85caf07ddbc3d696c3df66695298b420aea0f
Yisheng Gong <yisheng_gong@onmail.com>
2023-06-27 11:26:37 -0700

    update gorm exp

--- go/orm/go.mod
+++ go/orm/go.mod
@@ -1,4 +1,14 @@
-module gorm.io/driver/mysql
-module gorm.io/gorm
+module playground
+
+require (
+	gorm.io/driver/mysql v1.5.1
+	gorm.io/gorm v1.25.1
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
 
 go 1.19
--- go/orm/gorm_exp.go
+++ go/orm/gorm_exp.go
@@ -1,11 +1,13 @@
-package orm
+package main
 
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"fmt"
 )
 
 func main() {
-	dsn := "root:root@127.0.0.1:3306/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:root@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	fmt.Println(db, err)
 }
