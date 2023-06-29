3e848fe968dce4704251b022c580fecd6300db0c
Yisheng Gong <yisheng_gong@onmail.com>
2023-06-28 18:28:37 -0700

    add timeout func

--- python/asyncio/sub_process.py
+++ python/asyncio/sub_process.py
@@ -1,5 +1,15 @@
 import asyncio
 
+async def timeout_run_func(timeout_sec, func, *args, **kwargs):
+    async def wrap_func():
+        return await func(*args, **kwargs)
+
+    res = await asyncio.wait_for(
+        wrap_func(),
+        timeout=timeout_sec,
+    )
+    return res
+
 async def async_procedure(cmd):
     proc = await asyncio.create_subprocess_shell(cmd)
     stdout, stderr = await proc.communicate()
@@ -9,5 +19,11 @@ async def async_procedure(cmd):
     if stderr:
         print(f'[stderr]\n{stderr.decode()}')
 
+async def main(cmd):
+    try:
+        await timeout_run_func(1, async_procedure, cmd)
+    except asyncio.TimeoutError:
+        print('timeout')
+
 if __name__ == '__main__':
-    asyncio.run(async_procedure('sleep 2'))
+    asyncio.run(main('sleep 5'))
