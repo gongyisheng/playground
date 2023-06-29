7760f1cd939608977632aa182acaf11f39943c92
Yisheng Gong <yisheng_gong@onmail.com>
2023-06-29 11:07:35 -0700

    improve sub process

--- python/asyncio/sub_process.py
+++ python/asyncio/sub_process.py
@@ -8,13 +8,16 @@ async def get_running_process_number(pattern):
     stdout, stderr = await proc.communicate()
     return int(stdout.decode().strip())
 
-async def async_procedure(cmd):
+async def async_procedure(cmd, timeout=None):
     async with global_lock:
         start_proc_number = await get_running_process_number('python')
         print("start:",start_proc_number)
         proc = await asyncio.create_subprocess_shell(cmd)
         try:
-            stdout, stderr = await asyncio.wait_for(proc.communicate(), 5)
+            if timeout:
+                stdout, stderr = await asyncio.wait_for(proc.communicate(), timeout=timeout)
+            else:
+                stdout, stderr = await proc.communicate()
             print(f'[{cmd!r} exited with {proc.returncode}]')
             if stdout:
                 print(f'[stdout]\n{stdout.decode()}')
@@ -33,10 +36,10 @@ async def async_procedure(cmd):
                     break
                 await asyncio.sleep(10)
 
-async def main():
-    tasks = [async_procedure('sleep 1') for i in range(10)]
+async def main(cmd, timeout=None):
+    tasks = [async_procedure(cmd, timeout=timeout) for i in range(10)]
     await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    loop.run_until_complete(main('sleep 2', 1))
