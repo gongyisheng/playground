81d3e56660302497f0c44fb951b0da8c37a2d45a
Yisheng Gong <yisheng_gong@onmail.com>
2023-07-24 11:19:25 -0700

    improve async procedure

--- python/asyncio/sub_process.py
+++ python/asyncio/sub_process.py
@@ -1,43 +1,38 @@
 import asyncio
-import os
+import time
 
-global_lock = asyncio.Semaphore(2)
-
-async def get_running_process_number(pattern):
-    proc = await asyncio.create_subprocess_shell(f'ps aux | grep {pattern} -c', stdout=asyncio.subprocess.PIPE)
-    stdout, stderr = await proc.communicate()
-    return int(stdout.decode().strip())
+global_lock = asyncio.Semaphore(1)
 
 async def async_procedure(cmd, timeout=None):
     async with global_lock:
-        start_proc_number = await get_running_process_number('python')
-        print("start:",start_proc_number)
+        start = time.time()
         proc = await asyncio.create_subprocess_shell("exec "+cmd)
         try:
             if timeout:
-                stdout, stderr = await asyncio.wait_for(proc.communicate(), timeout=timeout)
+                await asyncio.wait_for(proc.wait(), timeout=timeout)
             else:
-                stdout, stderr = await proc.communicate()
+                await proc.wait()
             print(f'[{cmd!r} exited with {proc.returncode}]')
-            if stdout:
-                print(f'[stdout]\n{stdout.decode()}')
-            if stderr:
-                print(f'[stderr]\n{stderr.decode()}')
+        except asyncio.TimeoutError as e:
+            print(f'[{cmd!r} get timeout error]')
         except Exception as e:
-            pass
+            print(f'[{cmd!r} get uncaught exception {e}]')
         finally:
             if proc.returncode is None:
                 print("kill")
                 proc.kill()
-            end_proc_number = await get_running_process_number('python')
-            print("end:",end_proc_number)
+            end = time.time()
+            print(f"cost {end-start}")
 
 async def main(cmd, timeout=None):
     tasks = [async_procedure(cmd, timeout=timeout) for i in range(10)]
     await asyncio.gather(*tasks)
-    await asyncio.sleep(100)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     # loop.run_until_complete(main('sleep 3', 15))
-    loop.run_until_complete(main('python deadlock.py', 15))
+    # loop.run_until_complete(main('python deadlock.py', 15))
+    # loop.run_until_complete(main('sleep 3', 3.050))
+    # loop.run_until_complete(main('python3 -c \"from pdf import pdf2html\"', 3.050))
+    # loop.run_until_complete(main('pdf2htmlEX', 0.5))
+    loop.run_until_complete(main('sleep 1', 1.017))
