a82c24b8bf51bda4b38227210dea55f27297bfd1
Yisheng Gong <yisheng_gong@onmail.com>
2023-07-29 17:42:46 -0700

    add bs4 test

---
+++ python/bs4/attrs_copy.py
@@ -0,0 +1,19 @@
+from copy import deepcopy
+from bs4 import BeautifulSoup
+
+def main():
+    soup = BeautifulSoup('<b class="boldest">Extremely bold</b>', 'lxml')
+    s1 = soup.prettify()
+    original_attrs = []
+    for tag in soup.find_all(True):
+        original_attrs.append(deepcopy(tag.attrs))
+        tag.attrs['test'] = 'test'
+    i=0
+    for tag in soup.find_all(True):
+        tag.attrs = original_attrs[i]
+        i = i + 1
+    s2 = soup.prettify()
+    print(s1==s2)
+
+if __name__ == '__main__':
+    main()
