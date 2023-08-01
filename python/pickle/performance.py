d4a4973793e24f08a212efe3f2a5addbf596fb02
Yisheng Gong <yisheng_gong@onmail.com>
2023-08-01 14:07:59 -0700

    test pickle performance

---
+++ python/pickle/performance.py
@@ -0,0 +1,39 @@
+import pickle
+import json
+import time
+
+def main(round):
+    f = open("/Users/temp/Downloads/log4j-active (2).txt", 'rb')
+    content = f.read()
+
+    _tmp1 = None
+    _tmp2 = None
+
+    start = time.time()
+    for i in range(round):
+        _tmp1 = pickle.dumps(content)
+    end = time.time()
+    print(f"[pickle dump] cost {end-start} s")
+
+    start = time.time()
+    for i in range(round):
+        _tmp2 = pickle.loads(_tmp1)
+    end = time.time()
+    print(f"[pickle load]cost {end-start} s")
+    print(_tmp2==content)
+
+    start = time.time()
+    for i in range(round):
+        _tmp1 = json.dumps(content.decode('utf-8'))
+    end = time.time()
+    print(f"[json dump] cost {end-start} s")
+
+    start = time.time()
+    for i in range(round):
+        _tmp2 = json.loads(_tmp1).encode('utf-8')
+    end = time.time()
+    print(f"[json load]cost {end-start} s")
+    print(_tmp2==content)
+
+if __name__ == '__main__':
+    main(100)
