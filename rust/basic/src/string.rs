0b981b80656fcb523ae0dcf85ba1ac279e652c40
Yisheng Gong <yisheng_gong@onmail.com>
2023-08-10 00:43:33 -0700

    add string

---
+++ rust/basic/src/string.rs
@@ -0,0 +1,7 @@
+// "abc": This is a string literal, which is a fixed-size sequence of characters. 
+// String literals are stored in the program's binary and have a static lifetime. 
+// They are usually used for short, known strings that don't need to be mutated.
+
+// String::from("abc"): This creates a String type instance by dynamically allocating 
+// memory on the heap to store the string content. String is a dynamically resizable, 
+// heap-allocated string type that can be modified and grown.
