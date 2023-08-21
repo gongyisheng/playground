e36767b023001d0bf815df02aaac8b6865e77f51
Yisheng Gong <yisheng_gong@onmail.com>
2023-08-21 01:56:19 -0700

    improve oop

--- rust/basic/src/oop.rs
+++ rust/basic/src/oop.rs
@@ -3,23 +3,31 @@ pub struct Rectangle {
     pub height: u32,
 }
 
+// It is recommended to return -> &mut Self instead of nothing
+// because it returns the instance of the struct on which the
+// method is called, which allows us to chain methods
+
 impl Rectangle {
     pub fn area(&self) -> u32 {
         self.width * self.height
     }
 
-    pub fn set_width(&mut self, width: u32) {
+    pub fn set_width(&mut self, width: u32) -> &mut Self {
         // &mut self indicates that the method is allowed to modify 
         // the internal state of the instance of the struct on which 
         // the method is called
         self.width = width;
+
+        self
     }
 
-    pub fn set_height(&mut self, height: u32) {
+    pub fn set_height(&mut self, height: u32) -> &mut Self {
         // &mut self indicates that the method is allowed to modify 
         // the internal state of the instance of the struct on which 
         // the method is called
         self.height = height;
+
+        self
     }
 
     pub fn can_hold(&self, other: &Rectangle) -> bool {
