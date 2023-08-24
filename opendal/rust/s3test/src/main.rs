c216bf95c5bf48b0aa112222b229ce1e56150703
Yisheng Gong <yisheng_gong@onmail.com>
2023-08-24 01:17:05 -0700

    update s3 test

--- opendal/rust/s3test/src/main.rs
+++ opendal/rust/s3test/src/main.rs
@@ -5,6 +5,7 @@ use log::info;
 
 use config::{Config, File};
 
+use opendal::layers::LoggingLayer;
 use opendal::services::S3;
 use opendal::Operator;
 
@@ -18,22 +19,27 @@ async fn main() -> Result<()> {
         .build()
         .unwrap();
 
-    let ACCESS_KEY_ID = secrets.get_string(&"GCS_ACCESS_KEY_ID").ok().unwrap();
-    let SECRET_ACCESS_KEY = secrets.get_string(&"GCS_SECRET_ACCESS_KEY").ok().unwrap();
+    let access_key_id = secrets.get_string("GCS_ACCESS_KEY_ID").ok().unwrap();
+    let secret_access_key = secrets.get_string("GCS_SECRET_ACCESS_KEY").ok().unwrap();
     // Print out our settings (as a HashMap)
     let debug_secrets = secrets.try_deserialize::<HashMap<String, String>>().unwrap();
     println!("secrets: {:?}", debug_secrets);
 
     let mut builder = S3::default();
-    builder.root("/Users/temp/Downloads");
+    builder.root("o");
     builder.bucket("opendal-test");
-    builder.region("us-west1");
-    builder.endpoint("https://storage.googleapis.com");
-    builder.access_key_id(&ACCESS_KEY_ID);
-    builder.secret_access_key(&SECRET_ACCESS_KEY);
+    // builder.region("us-west1");
+    builder.endpoint("https://storage.googleapis.com/storage/v1/b");
+    // builder.access_key_id(&access_key_id);
+    // builder.secret_access_key(&secret_access_key);
     
-    let op = Operator::new(builder)?.finish();
+    let op = Operator::new(builder)?
+        .layer(LoggingLayer::default())
+        .finish();
     info!("operator: {:?}", op);
+
+    op.read("test.csv").await?;
+    //op.write("hello.txt", "Hello, World!").await?;
     
     Ok(())
 }
