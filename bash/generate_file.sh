440d1c323180c65579714106f4324582e1574328
Yisheng Gong <yisheng_gong@onmail.com>
2023-09-04 04:17:41 -0700

    add generate file bash script

--- bash/generate_file.sh
+++ bash/generate_file.sh
@@ -1,4 +1,4 @@
 for i in {1..1024}; do
-    echo "OpenDAL: access data freely.\n" >> "normal_file"
-    echo "OpenDAL: access data freely.\n" >> "special_file  !@#$%^&()_+-=;',"
+    echo "OpenDAL: access data freely." >> "normal_file"
+    echo "OpenDAL: access data freely." >> "special_file  !@#$%^&()_+-=;',"
 done
