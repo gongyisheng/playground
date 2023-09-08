38b6a444af2a122349a7ece8e8a6f5151ddcdacb
Yisheng Gong <yisheng_gong@onmail.com>
2023-09-08 12:06:05 -0700

    test overwrite

--- python/os/rename.py
+++ python/os/rename.py
@@ -3,13 +3,15 @@ import time
 
 def main():
     f = open('test.txt', 'w')
-    f.write('Hello, world!')
+    f.write('Hello, world!\n')
+    f.write('Hello, world!\n')
     f.close()
 
     time.sleep(2)
 
     f = open('test2.txt', 'w')
-    f.write('Hello, world!')
+    f.write('Hello, world2!\n')
+    f.write('Hello, world2!\n')
     f.close()
 
     time.sleep(2)
@@ -17,8 +19,6 @@ def main():
     os.rename('test2.txt', 'test.txt')
 
     time.sleep(2)
-
-    os.remove('test.txt')
 
 if __name__ == '__main__':
     main()
