d4abdde2832d301b7b9f4df15a88d2d15a04a543
Yisheng Gong <yisheng_gong@onmail.com>
2023-09-18 01:47:50 -0700

    add move loop

---
+++ python/mouse/move_loop.py
@@ -0,0 +1,14 @@
+from pynput.mouse import Button, Controller
+import time 
+import random
+
+def main():
+    mouse = Controller()
+    while True:
+        time.sleep(random.randint(1, 10))
+        mouse.position = (random.randint(0, 1440), random.randint(0, 900))
+        print ("Current position: " + str(mouse.position))
+    
+if __name__ == '__main__':
+    main()
+    
