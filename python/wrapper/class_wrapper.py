9a6bacd8b8e39633c5dd38b770337462fa3c74e8
Yisheng Gong <yisheng_gong@onmail.com>
2023-11-03 12:59:49 -0700

    add class wrapper test

---
+++ python/wrapper/class_wrapper.py
@@ -0,0 +1,26 @@
+import asyncio
+import functools
+
+class Example:
+
+    def another_func(self):
+        print("ANOTHER FUNC")
+    
+    def wrapper(func):
+        @functools.wraps(func)
+        async def wrap(self, *args, **kwargs):
+            print("inside wrap")
+            self.another_func()
+            return await func(self, *args, **kwargs)
+        return wrap
+
+    @wrapper
+    async def method(self):
+        print("METHOD")
+
+async def main():
+    e = Example()
+    await e.method()
+
+if __name__ == "__main__":
+    asyncio.run(main())
