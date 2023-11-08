29d29ff60b87aa7f6459f072624aab38dc4d350d
Yisheng Gong <yisheng_gong@onmail.com>
2023-11-08 14:00:13 -0800

    remove unused function

--- python/asyncio/generator_exit_error.py
+++ python/asyncio/generator_exit_error.py
@@ -14,9 +14,6 @@ class MyGenIt(object):
         if exc_type:
             logging.error("Aborted %s", self,
                           exc_info=(exc_type, exc_value, traceback))
-def generator():
-    for i in range(10):
-        yield i
 
 if __name__ == "__main__":
     for x in MyGenIt("foo",range(10)):
