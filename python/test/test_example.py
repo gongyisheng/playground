d23386706675d5f60f7528983d1116e42348df9a
Yisheng Gong <yisheng_gong@onmail.com>
2023-11-09 21:58:16 -0800

    add pytest example

---
+++ python/test/test_example.py
@@ -0,0 +1,12 @@
+def add(x, y):
+    return x + y
+
+def test_addition():
+    assert add(2, 3) == 5
+
+def test_addition_negative():
+    assert add(-1, 1) == 0
+
+# run command
+# pytest -v -s test_example.py
+# pytest -k test_addition_negative
