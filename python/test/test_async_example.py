3d057f82898375ce41cec84575a974c4615a573e
Yisheng Gong <yisheng_gong@onmail.com>
2023-11-09 22:09:04 -0800

    add async test example

---
+++ python/test/test_async_example.py
@@ -0,0 +1,17 @@
+import asyncio
+import pytest
+
+async def async_add(x, y):
+    await asyncio.sleep(1)
+    return x + y
+
+@pytest.mark.asyncio
+async def test_async_addition():
+    assert await async_add(2, 3) == 5
+
+@pytest.mark.asyncio
+async def test_async_addition_negative():
+    assert await async_add(-1, 1) == 0
+
+# This command will only run async tests:
+# pytest -k asyncio
