51e39079f0a5e343fcb7f1f864504b3ecfc0dd34
Yisheng Gong <yisheng_gong@onmail.com>
2023-12-21 15:14:28 -0800

    add std test

---
+++ python/file/stdout_read.py
@@ -0,0 +1,10 @@
+import sys
+
+def main():
+    print('Hello, world!', file=open('stdout_read.txt', 'w'))
+    print(sys.stdout.fileno())
+    print(sys.stdout.isatty())
+    print(sys.stdout.name)
+
+if __name__ == '__main__':
+    main()
