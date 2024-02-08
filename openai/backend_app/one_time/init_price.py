4b3cbdba23f75e4e0ab1bc65ebeadf519433e11a
Yisheng Gong <yisheng_gong@onmail.com>
2024-02-08 22:19:06 +0800

    add gpt-3.5-turbo-0125

--- openai/backend_app/one_time/init_price.py
+++ openai/backend_app/one_time/init_price.py
@@ -17,3 +17,7 @@ if pricing_model.get_current_pricing_by_model("gpt-4-0125-preview") is None:
     pricing_model.create_pricing(
         "gpt-4-0125-preview", 0.01 / 1000, 0.03 / 1000, 0
     )
+if pricing_model.get_current_pricing_by_model("gpt-3.5-turbo-0125") is None:
+    pricing_model.create_pricing(
+        "gpt-3.5-turbo-0125", 0.0005 / 1000, 0.0015 / 1000, 0
+    )
