ee6dce7e36c26e02f0ea038f5ff541bbdcf29b02
Yisheng Gong <yisheng_gong@onmail.com>
2024-04-16 01:55:32 -0700

    use iwconfig

--- raspberrypi/scripts/autowifi.py
+++ raspberrypi/scripts/autowifi.py
@@ -5,8 +5,8 @@ import time
 now = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
 
 def main():
-    res = os.popen('ifconfig | grep wlan0').read()
-    if 'wlan0' not in res:
+    res = os.popen('iwconfig | grep wlan0').read()
+    if 'Bustersvision' not in res:
         print(f'[{now}] wifi is down, restart...')
         os.system('sudo ip link set wlan0 up')
     else:
