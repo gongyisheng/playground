9624e70bd8335cc54b2b13dd02ad956377a07bdc
Yisheng Gong <yisheng_gong@onmail.com>
2024-04-17 00:22:47 -0700

    add random http server

---
+++ raspberrypi/scripts/random_http_server.py
@@ -0,0 +1,44 @@
+import tornado.ioloop
+import tornado.web
+import random
+import string
+
+def get_random_string(length: int) -> str:
+    # Generate a random string
+    return ''.join(random.choices(string.ascii_letters, k=length))
+
+class BaseHandler(tornado.web.RequestHandler):
+
+    def set_default_headers(self) -> None:
+        # Allow specific headers
+        self.set_header("Access-Control-Allow-Headers", "Content-Type")
+
+        # Allow specific methods
+        self.set_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    
+    def options(self):
+        # Handle preflight OPTIONS requests
+        self.set_status(204)
+        self.finish()
+    
+    def get(self):
+        length = random.randint(1, 1024*1024) # 1 MB
+        self.write(get_random_string(length))
+        self.set_status(200)
+        self.finish()
+    
+    def post(self):
+        length = random.randint(1, 1024*1024) # 1 MB
+        self.write(get_random_string(length))
+        self.set_status(200)
+        self.finish()
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", BaseHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
