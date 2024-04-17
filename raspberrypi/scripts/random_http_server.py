5745886cf25a9ea214f9e20cc95b6945d34435e9
Yisheng Gong <yisheng_gong@onmail.com>
2024-04-17 14:25:58 -0700

    add log

--- raspberrypi/scripts/random_http_server.py
+++ raspberrypi/scripts/random_http_server.py
@@ -41,4 +41,5 @@ def make_app():
 if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
+    print('Server started at http://localhost:8888')
     tornado.ioloop.IOLoop.current().start()
