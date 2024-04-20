4c8d4411e8164b9ec435b8d80b07d0c4eeb07481
Yisheng Gong <yisheng_gong@onmail.com>
2024-04-20 14:08:00 -0700

    add new model

--- llm/prompt_tool/backend_app/one_time/init_price.py
+++ llm/prompt_tool/backend_app/one_time/init_price.py
@@ -16,3 +16,5 @@ if pricing_model.get_current_pricing_by_model("gpt-4-0125-preview") is None:
     pricing_model.create_pricing("gpt-4-0125-preview", 0.01 / 1000, 0.03 / 1000, 0)
 if pricing_model.get_current_pricing_by_model("gpt-3.5-turbo-0125") is None:
     pricing_model.create_pricing("gpt-3.5-turbo-0125", 0.0005 / 1000, 0.0015 / 1000, 0)
+if pricing_model.get_current_pricing_by_model("gpt-4-turbo-2024-04-09") is None:
+    pricing_model.create_pricing("gpt-4-turbo-2024-04-09", 10 / 1000000, 30 / 1000000, 0)
