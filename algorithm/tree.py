e59287c18508a0d20614c71adda99e929ef9b62b
Yisheng Gong <yisheng_gong@onmail.com>
2024-05-29 01:04:36 -0700

    format code

--- algorithm/tree.py
+++ algorithm/tree.py
@@ -1,10 +1,25 @@
 from typing import List
 
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+
+# Helper function to build a binary tree from a list of values
+def build_tree(vals: List[int]) -> TreeNode:
+    def build_tree_helper(vals: List[int], idx: int) -> TreeNode:
+        if idx >= len(vals) or vals[idx] is None:
+            return None
+        node = TreeNode(vals[idx])
+        node.left = build_tree_helper(vals, 2 * idx + 1)
+        node.right = build_tree_helper(vals, 2 * idx + 2)
+        return node
+
+    return build_tree_helper(vals, 0)
+
 
 # LC 144
 def preorder_traversal(root: TreeNode) -> List[int]:
@@ -16,6 +31,7 @@ def preorder_traversal(root: TreeNode) -> List[int]:
     vals.extend(preorder_traversal(root.right))
     return vals
 
+
 # LC 94
 def inorder_traversal(root: TreeNode) -> List[int]:
     if root is None:
@@ -26,6 +42,7 @@ def inorder_traversal(root: TreeNode) -> List[int]:
     vals.extend(inorder_traversal(root.right))
     return vals
 
+
 # LC 145
 def postorder_traversal(root: TreeNode) -> List[int]:
     if root is None:
@@ -35,6 +52,7 @@ def postorder_traversal(root: TreeNode) -> List[int]:
     vals.extend(postorder_traversal(root.right))
     vals.append(root.val)
     return vals
+
 
 # LC 102
 def levelorder_traversal(root: TreeNode) -> List[int]:
@@ -51,15 +69,6 @@ def levelorder_traversal(root: TreeNode) -> List[int]:
             queue.append(node.right)
     return vals
 
-def build_tree(vals: List[int]) -> TreeNode:
-    def build_tree_helper(vals: List[int], idx: int) -> TreeNode:
-        if idx >= len(vals) or vals[idx] is None:
-            return None
-        node = TreeNode(vals[idx])
-        node.left = build_tree_helper(vals, 2 * idx + 1)
-        node.right = build_tree_helper(vals, 2 * idx + 2)
-        return node
-    return build_tree_helper(vals, 0)
 
 if __name__ == "__main__":
     vals = [1, 2, 3, None, 4, 5, 6]
