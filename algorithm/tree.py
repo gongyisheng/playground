19efbcb32b7256dbc561e233f1da898ea3d00b7d
Yisheng Gong <yisheng_gong@onmail.com>
2024-05-29 01:22:36 -0700

    rewrite levelorder

--- algorithm/tree.py
+++ algorithm/tree.py
@@ -1,4 +1,5 @@
 from typing import List
+import collections
 
 
 class TreeNode:
@@ -7,9 +8,14 @@ class TreeNode:
         self.left = left
         self.right = right
 
+class NrayTreeNode:
+    def __init__(self, val=0, children=None):
+        self.val = val
+        self.children = children if children else []
+
 
 # Helper function to build a binary tree from a list of values
-def build_tree(vals: List[int]) -> TreeNode:
+def build_binary_tree(vals: List[int]) -> TreeNode:
     def build_tree_helper(vals: List[int], idx: int) -> TreeNode:
         if idx >= len(vals) or vals[idx] is None:
             return None
@@ -19,6 +25,18 @@ def build_tree(vals: List[int]) -> TreeNode:
         return node
 
     return build_tree_helper(vals, 0)
+
+# def build_nray_tree(n: int, vals: List[int]) -> NrayTreeNode:
+#     def build_tree_helper(vals: List[int], idx: int) -> NaryTreeNode:
+#         if idx >= len(vals) or vals[idx] is None:
+#             return None
+#         node = NaryTreeNode(vals[idx])
+#         for i in range(1, n+1):
+#             child = build_tree_helper(vals, n*idx + i)
+#             node.children.append(child)
+#         return node
+
+#     return build_tree_helper(vals, 0)
 
 
 # LC 144
@@ -55,24 +73,42 @@ def postorder_traversal(root: TreeNode) -> List[int]:
 
 
 # LC 102
-def levelorder_traversal(root: TreeNode) -> List[int]:
+def levelorder_traversal(root: TreeNode) -> List[List[int]]:
+    if root is None:
+        return []
     vals = []
-    queue = []
-    if root:
-        queue.append(root)
+    queue = collections.deque([root])
     while queue:
-        node = queue.pop(0)
-        vals.append(node.val)
-        if node.left:
-            queue.append(node.left)
-        if node.right:
-            queue.append(node.right)
+        level = []
+        for _ in range(len(queue)):
+            node = queue.popleft()
+            level.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        vals.append(level)
     return vals
+
+# LC 429
+def levelorder_traversal_nray(root: NrayTreeNode) -> List[List[int]]:
+    if root is None:
+        return []
+    result = []
+    queue = collections.deque([root])
+    while queue:
+        level = []
+        for _ in range(len(queue)):
+            node = queue.popleft()
+            level.append(node.val)
+            queue.extend(node.children)
+        result.append(level)
+    return result
 
 
 if __name__ == "__main__":
     vals = [1, 2, 3, None, 4, 5, 6]
-    root = build_tree(vals)
+    root = build_binary_tree(vals)
     print("preorder: ", preorder_traversal(root))
     print("inorder: ", inorder_traversal(root))
     print("postorder: ", postorder_traversal(root))
