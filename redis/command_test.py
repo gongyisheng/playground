f2b4d0ab9b7ea73cf34495c5d358d69cc4be3c9e
Yisheng Gong <yisheng_gong@onmail.com>
2024-06-03 22:07:47 -0700

    add exists test

--- redis/command_test.py
+++ redis/command_test.py
@@ -9,6 +9,17 @@ def test_hkeys():
     val = r.hkeys("test")
     print(val)
 
+# exists
+def test_exists():
+    r = redis.Redis(host="localhost", port=6379, db=0)
+    r.hset("test", "test_field1", "1")
+    val = bool(r.exists("test"))
+    print(val)
+    val = bool(r.exists("test1"))
+    print(val)
+
+
 
 if __name__ == "__main__":
-    test_hkeys()
+    #test_hkeys()
+    test_exists()
