e23d524ed01ff350d4abb4e4d830a0777f815b97
Yisheng Gong <yisheng_gong@onmail.com>
2024-06-11 16:10:34 -0700

    format code

--- algorithm/sort.py
+++ algorithm/sort.py
@@ -11,11 +11,12 @@ def quick_sort(arr):
             right.append(num)
     return quick_sort(left) + [piviot] + quick_sort(right)
 
+
 def merge_sort(arr):
     def merge(left, right):
         result = []
         i = j = 0
-        
+
         while i < len(left) and j < len(right):
             if left[i] < right[j]:
                 result.append(left[i])
@@ -23,26 +24,27 @@ def merge_sort(arr):
             else:
                 result.append(right[j])
                 j += 1
-        
+
         result.extend(left[i:])
         result.extend(right[j:])
-        
+
         return result
+
     if len(arr) <= 1:
         return arr
-    
+
     # Split the array into two halves
     mid = len(arr) // 2
     left_half = arr[:mid]
     right_half = arr[mid:]
-    
+
     left_half = merge_sort(left_half)
     right_half = merge_sort(right_half)
-    
+
     return merge(left_half, right_half)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     arr = [5, 3, 7, 2, 8, 4, 1, 6]
     print(quick_sort(arr))
     print(merge_sort(arr))
