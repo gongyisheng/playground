ea2d478b54b3be974f95d978f64ba246c40f52c4
Yisheng Gong <yisheng_gong@onmail.com>
2024-06-19 02:50:58 -0700

    use semaphore

--- network/lvs_case/polling.py
+++ network/lvs_case/polling.py
@@ -1,13 +1,15 @@
 import asyncio
 from redis import asyncio as aioredis
 
-async def set_key(redis, key, value):
-    await redis.set(key, value)
+async def set_key(redis:aioredis.Redis, semaphore: asyncio.Semaphore, key, value):
+    async with semaphore:
+        await redis.set(key, value)
 
 async def main():
     connection_pool = aioredis.BlockingConnectionPool(max_connections=10)
     redis = aioredis.Redis(host="127.0.0.1", port=8080, connection_pool=connection_pool)
-    tasks = [asyncio.create_task(set_key(redis, f'key_{i}', f'value_{i}')) for i in range(2<<20)]
+    semaphore = asyncio.Semaphore(50)
+    tasks = [asyncio.create_task(set_key(redis, semaphore, f'key_{i}', f'value_{i}')) for i in range(2<<10)]
     await asyncio.gather(*tasks)
     await asyncio.sleep(600)
 
