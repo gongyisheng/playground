e401ec9e6d70e81f0bc1aad250733157736f5f1f
Yisheng Gong <yisheng_gong@onmail.com>
2024-06-19 02:53:54 -0700

    increase concurrency

--- network/lvs_case/polling.py
+++ network/lvs_case/polling.py
@@ -9,7 +9,7 @@ async def main():
     connection_pool = aioredis.BlockingConnectionPool(max_connections=10)
     redis = aioredis.Redis(host="127.0.0.1", port=8080, connection_pool=connection_pool)
     semaphore = asyncio.Semaphore(50)
-    tasks = [asyncio.create_task(set_key(redis, semaphore, f'key_{i}', f'value_{i}')) for i in range(2<<10)]
+    tasks = [asyncio.create_task(set_key(redis, semaphore, f'key_{i}', f'value_{i}')) for i in range(2<<20)]
     await asyncio.gather(*tasks)
     await asyncio.sleep(600)
 
