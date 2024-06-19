39402af7ee6b0a1173c89251146080dbe2a87a53
Yisheng Gong <yisheng_gong@onmail.com>
2024-06-19 02:43:06 -0700

    add sleep

--- network/lvs_case/polling.py
+++ network/lvs_case/polling.py
@@ -9,6 +9,7 @@ async def main():
     redis = aioredis.Redis(host="127.0.0.1", port=8080, connection_pool=connection_pool)
     tasks = [asyncio.create_task(set_key(redis, f'key_{i}', f'value_{i}')) for i in range(20)]
     await asyncio.gather(*tasks)
+    await asyncio.sleep(600)
 
 if __name__ == '__main__':
     asyncio.run(main())
