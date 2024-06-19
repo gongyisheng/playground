21319113632db533bf5bcb632da515af4ba92e86
Yisheng Gong <yisheng_gong@onmail.com>
2024-06-19 02:42:35 -0700

    use blocking pool

--- network/lvs_case/polling.py
+++ network/lvs_case/polling.py
@@ -5,8 +5,8 @@ async def set_key(redis, key, value):
     await redis.set(key, value)
 
 async def main():
-    connection_pool = aioredis.ConnectionPool(host="127.0.0.1", port=8080, max_connections=10)
-    redis = aioredis.Redis(connection_pool=connection_pool)
+    connection_pool = aioredis.BlockingConnectionPool(max_connections=10)
+    redis = aioredis.Redis(host="127.0.0.1", port=8080, connection_pool=connection_pool)
     tasks = [asyncio.create_task(set_key(redis, f'key_{i}', f'value_{i}')) for i in range(20)]
     await asyncio.gather(*tasks)
 
