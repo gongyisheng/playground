49c3ac5e0f07ad1c9f9242d22175ebb6baa1a9c8
Yisheng Gong <yisheng_gong@onmail.com>
2024-06-19 03:14:20 -0700

    increase conn

--- network/lvs_case/polling.py
+++ network/lvs_case/polling.py
@@ -6,7 +6,7 @@ async def set_key(redis:aioredis.Redis, semaphore: asyncio.Semaphore, key, value
         await redis.set(key, value)
 
 async def main():
-    connection_pool = aioredis.BlockingConnectionPool(host="127.0.0.1", port=8080, max_connections=1)
+    connection_pool = aioredis.BlockingConnectionPool(host="127.0.0.1", port=8080, max_connections=10)
     redis = aioredis.Redis(connection_pool=connection_pool)
     semaphore = asyncio.Semaphore(50)
     tasks = [asyncio.create_task(set_key(redis, semaphore, f'key_{i}', f'value_{i}')) for i in range(2<<10)]
