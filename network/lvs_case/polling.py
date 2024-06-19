edf9e9a9e3bc81fd64de562eb581fc42dcd1def6
Yisheng Gong <yisheng_gong@onmail.com>
2024-06-19 04:18:18 -0700

    update polling script

--- network/lvs_case/polling.py
+++ network/lvs_case/polling.py
@@ -1,19 +1,19 @@
 import asyncio
 from redis import asyncio as aioredis
 
-async def set_key(redis:aioredis.Redis, semaphore: asyncio.Semaphore, key, value):
-    async with semaphore:
-        await redis.set(key, value)
-        print(f'set key {key}')
+async def set_key(redis:aioredis.Redis, key, value):
+    await redis.set(key, value)
+    print(f'set key {key}')
 
 async def main():
     connection_pool = aioredis.BlockingConnectionPool(host="127.0.0.1", port=8080, max_connections=200)
     redis = aioredis.Redis(connection_pool=connection_pool)
-    semaphore = asyncio.Semaphore(200)
-    tasks = [asyncio.create_task(set_key(redis, semaphore, f'key_{i}', f'value_{i}')) for i in range(2<<15)]
-    await asyncio.gather(*tasks)
-    print('done set key')
-    await asyncio.sleep(600)
+    count = 0
+    while True:
+        tasks = [asyncio.create_task(set_key(redis, f'key_{i}', f'value_{i}')) for i in range(2<<10)]
+        await asyncio.gather(*tasks)
+        print(f'done set key round {count}')
+        count += 1
 
 if __name__ == '__main__':
     asyncio.run(main())
