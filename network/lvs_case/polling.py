77431057f37f5acea345559d20e84d8e3157999f
Yisheng Gong <yisheng_gong@onmail.com>
2024-06-19 16:16:04 -0700

    increase time interval

--- network/lvs_case/polling.py
+++ network/lvs_case/polling.py
@@ -19,7 +19,7 @@ async def main():
         await asyncio.gather(*tasks)
         print(f'done set key round {count}')
         count += 1
-        await asyncio.sleep(3)
+        await asyncio.sleep(3.5)
 
 if __name__ == '__main__':
     asyncio.run(main())
