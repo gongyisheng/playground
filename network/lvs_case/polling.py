53a7bd5ab581ac491808f875cb39909dd712bd73
Yisheng Gong <yisheng_gong@onmail.com>
2024-06-19 02:46:43 -0700

    increase task

--- network/lvs_case/polling.py
+++ network/lvs_case/polling.py
@@ -7,7 +7,7 @@ async def set_key(redis, key, value):
 async def main():
     connection_pool = aioredis.BlockingConnectionPool(max_connections=10)
     redis = aioredis.Redis(host="127.0.0.1", port=8080, connection_pool=connection_pool)
-    tasks = [asyncio.create_task(set_key(redis, f'key_{i}', f'value_{i}')) for i in range(2000)]
+    tasks = [asyncio.create_task(set_key(redis, f'key_{i}', f'value_{i}')) for i in range(2<<20)]
     await asyncio.gather(*tasks)
     await asyncio.sleep(600)
 
