52c02094ac76136e44d2c6e076f710ecbc3b74de
Yisheng Gong <yisheng_gong@onmail.com>
2024-08-03 00:30:30 -0700

    add predict case

--- nlp/cooking-stackexchange/validate.py
+++ nlp/cooking-stackexchange/validate.py
@@ -2,3 +2,6 @@ import fasttext
 
 model = fasttext.load_model("cooking_model.bin")
 model.test("cooking.valid", k=5)
+
+model.predict("Why not put knives in the dishwasher?")
+model.predict("Which baking dish is best to bake a banana bread ?")
