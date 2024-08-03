d187cfff120aac9382cec152930e3bca336631aa
Yisheng Gong <yisheng_gong@onmail.com>
2024-08-03 00:32:09 -0700

    use print

--- nlp/cooking-stackexchange/validate.py
+++ nlp/cooking-stackexchange/validate.py
@@ -1,7 +1,7 @@
 import fasttext
 
 model = fasttext.load_model("cooking_model.bin")
-model.test("cooking.valid", k=5)
+print(model.test("cooking.valid", k=5))
 
-model.predict("Why not put knives in the dishwasher?")
-model.predict("Which baking dish is best to bake a banana bread ?")
+print(model.predict("Why not put knives in the dishwasher?"))
+print(model.predict("Which baking dish is best to bake a banana bread ?"))
