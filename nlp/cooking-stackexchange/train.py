4e5104ae2e9b92fde0172b1c271a62ebbf2a1571
gongyisheng <yisheng_gong@onmail.com>
2024-08-03 11:48:26 +0000

    add more steps

--- nlp/cooking-stackexchange/train.py
+++ nlp/cooking-stackexchange/train.py
@@ -1,4 +1,4 @@
 import fasttext
 
-model = fasttext.train_supervised(input="cooking.train", epoch=25)
+model = fasttext.train_supervised(input="cooking.train", lr=0.5, epoch=50, wordNgrams=2, bucket=200000, dim=50, loss='ova')
 model.save_model("cooking_model.bin")
