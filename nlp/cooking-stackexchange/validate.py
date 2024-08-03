b3c961de5b953e53aec156332fc1ca6eaa93fb50
Yisheng Gong <yisheng_gong@onmail.com>
2024-08-03 00:33:12 -0700

    use k=5

--- nlp/cooking-stackexchange/validate.py
+++ nlp/cooking-stackexchange/validate.py
@@ -3,5 +3,5 @@ import fasttext
 model = fasttext.load_model("cooking_model.bin")
 print(model.test("cooking.valid", k=5))
 
-print(model.predict("Why not put knives in the dishwasher?"))
-print(model.predict("Which baking dish is best to bake a banana bread ?"))
+print(model.predict("Why not put knives in the dishwasher?", k=5))
+print(model.predict("Which baking dish is best to bake a banana bread?", k=5))
