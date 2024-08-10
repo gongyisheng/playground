46f287360b2568cf91c5deea4314b6f00662571d
gongyisheng <yisheng_gong@onmail.com>
2024-08-10 09:53:49 +0000

    improve wordvec inspect

--- nlp/enwiki/wordvec.py
+++ nlp/enwiki/wordvec.py
@@ -2,7 +2,8 @@ import fasttext
 model = fasttext.load_model("enwiki_small.bin")
 
 # It returns all words in the vocabulary, sorted by decreasing frequency.
-print(model.words[:25])
+print("Length of model words: ", len(model.words))
+print("First 25 words: ", model.words[:25])
 
 # Get the word vector
-print(model.get_word_vector("the"))
+print(model.get_word_vector("meow"))
