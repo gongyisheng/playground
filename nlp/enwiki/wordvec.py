0b242ee4bb258310f48853090f78a354521c6a50
gongyisheng <yisheng_gong@onmail.com>
2024-08-10 10:55:10 +0000

    add get neighbors

--- nlp/enwiki/wordvec.py
+++ nlp/enwiki/wordvec.py
@@ -2,8 +2,17 @@ import fasttext
 model = fasttext.load_model("enwiki_small.bin")
 
 # It returns all words in the vocabulary, sorted by decreasing frequency.
-print("Length of model words: ", len(model.words))
-print("First 25 words: ", model.words[:25])
+w = model.words
+print("Length of model words: ", len(w))
+print("First 25 words: ", w[:25])
 
 # Get the word vector
-print(model.get_word_vector("meow"))
+v = model.get_word_vector("meow")
+print("Length of word vector: ", len(v))
+print("Word vector: ", v)
+
+# Get nearest neighbors
+print("Nearest neighbors of orange: ", model.get_nearest_neighbors('orange', k=20))
+print("Nearest neighbors of cat: ", model.get_nearest_neighbors('cat', k=20))
+
+# Measure similarity
