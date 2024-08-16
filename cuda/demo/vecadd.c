ac61a5e63a81b97e307c8e8a698cd9dc0f4c22ac
gongyisheng <yisheng_gong@onmail.com>
2024-08-16 07:53:58 +0000

    add vecadd c/cuda code

---
+++ cuda/demo/vecadd.c
@@ -0,0 +1,26 @@
+#define N 10000000
+#include <stdlib.h>
+
+void vector_add(float *out, float *a, float *b, int n) {
+  for (int i = 0; i < n; i++) {
+    out[i] = a[i] + b[i];
+  }
+}
+
+int main() {
+  float *a, *b, *out;
+
+  // Allocate memory
+  a = (float *)malloc(sizeof(float) * N);
+  b = (float *)malloc(sizeof(float) * N);
+  out = (float *)malloc(sizeof(float) * N);
+
+  // Initialize array
+  for (int i = 0; i < N; i++) {
+    a[i] = 1.0f;
+    b[i] = 2.0f;
+  }
+
+  // Main function
+  vector_add(out, a, b, N);
+}
