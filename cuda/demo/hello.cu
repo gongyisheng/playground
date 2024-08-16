c620fe716407013252f455430fcc4c030f8d84fd
gongyisheng <yisheng_gong@onmail.com>
2024-08-16 06:22:33 +0000

    add hello.cu/hello.cu

---
+++ cuda/demo/hello.cu
@@ -0,0 +1,11 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+__global__ void cuda_hello(){
+    printf("Hello World from GPU!\n");
+}
+
+int main() {
+    cuda_hello<<<1,1>>>(); 
+    return 0;
+}
