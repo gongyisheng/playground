7b6a569ed90aad3f687de14f8e7683b806d1a590
gongyisheng <yisheng_gong@onmail.com>
2024-09-23 21:31:21 +0000

    add rel

---
+++ nlp/rel/hello_world.py
@@ -0,0 +1,27 @@
+from REL.mention_detection import MentionDetection
+from REL.utils import process_results
+from REL.entity_disambiguation import EntityDisambiguation
+from REL.ner import Cmns, load_flair_ner
+
+wiki_version = "wiki_2014"
+base_url = "C:/path/to/rel_data"
+
+input_text = {
+    "my_doc": ("Hello, world!", []),
+}
+
+mention_detection = MentionDetection(base_url, wiki_version)
+tagger_ner = load_flair_ner("ner-fast")
+
+tagger_ngram = Cmns(base_url, wiki_version, n=5)
+mentions, n_mentions = mention_detection.find_mentions(input_text, tagger_ngram)
+
+config = {
+    "mode": "eval",
+    "model_path": "ed-wiki-2014",
+}
+model = EntityDisambiguation(base_url, wiki_version, config)
+
+predictions, timing = model.predict(mentions)
+result = process_results(mentions, predictions, input_text)
+print(result)
