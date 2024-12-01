f5680c2f547996a01c83224a15ca2ef78529850e
gongyisheng <yishenggong9437@gmail.com>
2024-11-30 16:44:48 -0800

    improve bench script

--- sysbench/run_mysql_bench.py
+++ sysbench/run_mysql_bench.py
@@ -1,17 +1,50 @@
 import subprocess
 
-def run_sysbench():
-    # Define the sysbench command
-    command = ["sysbench", "--test=cpu", "--cpu-max-prime=20000", "run"]
+def build_commands(user, password, database, bench_name, threads):
+    prep_command = ["sysbench", "--db-driver=mysql", "--mysql-host=127.0.0.1", "--mysql-port=3306", f"--mysql-user={user}", f"--mysql-password={password}", f"--mysql-db={database}", bench_name, "prepare"]
+    run_command = ["sysbench", "--db-driver=mysql", "--mysql-host=127.0.0.1", "--mysql-port=3306", f"--mysql-user={user}", f"--mysql-password={password}", f"--mysql-db={database}", bench_name, "run", f"--threads={threads}"]
+    clean_command = ["sysbench", "--db-driver=mysql", "--mysql-host=127.0.0.1", "--mysql-port=3306", f"--mysql-user={user}", f"--mysql-password={password}", f"--mysql-db={database}", bench_name, "cleanup"]
+    return prep_command, run_command, clean_command
 
-    # Execute the sysbench command and capture the output
-    result = subprocess.run(command, capture_output=True, text=True)
+def run_sysbench(user, password, database):
+    read_bench_set = [
+        "covering_index_scan",
+        "index_join",
+        "index_join_scan",
+        "oltp_point_select",
+        "oltp_read_only",
+        "select_random_points",
+        "select_random_ranges",
+        "index_scan",
+        "groupby_scan",
+        "table_scan",
+        "types_table_scan"
+    ]
+    write_bench_set = [
+        "oltp_delete_insert",
+        "oltp_insert",
+        "oltp_read_write",
+        "oltp_update_index",
+        "oltp_update_non_index",
+        "oltp_write_only",
+        "types_delete_insert",
+    ]
 
-    # Print the output
-    if result.returncode == 0:
-        print("Sysbench CPU Test Output:\n" + result.stdout)
-    else:
-        print("An error occurred while executing sysbench:\n" + result.stderr)
+    threads = [1, 2, 4, 8, 16, 32, 64]
+    for bench_name in read_bench_set:
+        for thread in threads:
+            prep_command, run_command, clean_command = build_commands(user, password, database, bench_name, thread)
+            print(f"Running {bench_name} with {thread} threads")
+            subprocess.run(prep_command, capture_output=True, text=True)
+            result = subprocess.run(run_command, capture_output=True, text=True)
+            subprocess.run(clean_command, capture_output=True, text=True)
+            print(result.stderr)
+            break
+        break
 
 if __name__ == "__main__":
-    run_sysbench()
+    import sys
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3] if len(sys.argv) > 3 else "sbtest"
+    run_sysbench(user, password, database)
