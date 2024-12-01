e39ecd0f159709570023e8c7f94145bb3e9d6eac
gongyisheng <yishenggong9437@gmail.com>
2024-11-30 17:14:10 -0800

    add test display

--- sysbench/run_mysql_bench.py
+++ sysbench/run_mysql_bench.py
@@ -7,10 +7,23 @@ def build_commands(user, password, database, table_size, bench_name, threads):
     clean_command = ["sysbench", "--db-driver=mysql", "--mysql-host=127.0.0.1", "--mysql-port=3306", f"--mysql-user={user}", f"--mysql-password={password}", f"--mysql-db={database}", bench_name, "cleanup"]
     return prep_command, run_command, clean_command
 
-def run_sysbench(user, password, database, table_size):
-    
-    print(f"Running sysbench benchmarks: database={database}, table_size={table_size}")
+def display_results(results):
+    print("Results:")
+    METHOD_LENGTH = max([len(method) for method in results.keys()] + [len("Method")])
+    THREAD_LENGTH = max([len(str(thread)) for thread_results in results.values() for thread in thread_results.keys()] + [len("Threads")])
+    READ_LENGTH = max([len(str(values['read'])) for thread_results in results.values() for values in thread_results.values()] + [len("Read QPS")])
+    WRITE_LENGTH = max([len(str(values['write'])) for thread_results in results.values() for values in thread_results.values()] + [len("Write QPS")])
+    TRANSACTION_LENGTH = max([len(str(values['transaction'])) for thread_results in results.values() for values in thread_results.values()] + [len("Transaction QPS")])
+    first_line = f"| Method{' '*(METHOD_LENGTH - len('Method'))} | Threads{' '*(THREAD_LENGTH - len('Threads'))} | Read QPS{' '*(READ_LENGTH - len('Read QPS'))} | Write QPS{' '*(WRITE_LENGTH - len('Write QPS'))} | Transaction QPS{' '*(TRANSACTION_LENGTH - len('Transaction QPS'))} |"
+    second_line = f"|{'-'*(METHOD_LENGTH + 2)}|{'-'*(THREAD_LENGTH + 2)}|{'-'*(READ_LENGTH + 2)}|{'-'*(WRITE_LENGTH + 2)}|{'-'*(TRANSACTION_LENGTH + 2)}|"
+    print(first_line)
+    print(second_line)
+    for bench_name, thread_results in results.items():
+        for thread, values in sorted(thread_results.items(), key=lambda x: x[0]):
+            line = f"| {bench_name}{' '*(METHOD_LENGTH - len(bench_name))} | {thread}{' '*(THREAD_LENGTH - len(str(thread)))} | {values['read']}{' '*(READ_LENGTH - len(str(values['read'])))} | {values['write']}{' '*(WRITE_LENGTH - len(str(values['write'])))} | {values['transaction']}{' '*(TRANSACTION_LENGTH - len(str(values['transaction'])))} |"
+            print(line)
 
+def run_sysbench(user, password, database, table_size):
     read_bench_set = [
         "oltp_point_select",
         "oltp_read_only",
@@ -33,6 +46,9 @@ def run_sysbench(user, password, database, table_size):
     transaction_pattern = r'transactions:\s+(\d+)'
     time_pattern = r'total time:\s+([\d.]+)s'
 
+    print(f"Running sysbench benchmarks: database={database}, table_size={table_size}")
+    results = {}
+
     threads = [1, 2, 4, 8, 16, 32, 64]
     for bench_name in read_bench_set + write_bench_set:
         for thread in threads:
@@ -53,6 +69,7 @@ def run_sysbench(user, password, database, table_size):
             print("Transaction QPS:", transaction/time)
             break
         break
+
 
 if __name__ == "__main__":
     import sys
