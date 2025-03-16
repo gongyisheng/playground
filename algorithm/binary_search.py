5d0eb8ef8d2812106ff32591c3b2f7400c2e856c
gongyisheng <yishenggong9437@gmail.com>
2025-03-15 17:55:19 -0700

    rename file; add note

--- algorithm/binary_search.py
+++ algorithm/binary_search.py
@@ -1,3 +1,10 @@
+# binary search questions
+# LC 2560, LC 2226, LC 1802, LC 1539, LC 1482, LC 1283, LC 1231, LC 1011, LC 875, LC 774, LC 410
+
+# Note:
+# 1. some dfs problems can be turned into binary search problems. eg, LC 2594
+# 2. if the given number is very big, it hints that we should use binary search.
+
 def binary_search(sorted_array, target):
     left = 0
     right = len(sorted_array) - 1
