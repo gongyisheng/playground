fa82bed286388d2bddabcb1ebb8da83ddb472f30
gongyisheng <yishenggong9437@gmail.com>
2025-04-21 23:34:46 -0700

    add rand5 to rand 7

--- algorithm/math_ops.py
+++ algorithm/math_ops.py
@@ -1,3 +1,5 @@
+import random
+
 def divide(x, y):
     sign = -1 if (x > 0) ^ (y > 0) else 1
     x, y = abs(x), abs(y)
@@ -13,5 +15,15 @@ def divide(x, y):
         x -= y << p
     return sign * res
 
+def rand5():
+    return random.randint(0, 4)
+
+def rand5_to_rand7():
+    while True:
+        num = 5 * rand5() + rand5()
+        if num < 21:
+            return num % 7
+
 if __name__ == "__main__":
     print(divide(10, -3))
+    print(rand5_to_rand7())
