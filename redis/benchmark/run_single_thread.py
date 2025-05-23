7ec3d2ed6d0b48791dbf553a42abaaab3c3c8a63
gongyisheng <yishenggong9437@gmail.com>
2025-05-23 01:16:54 -0700

    update host and port

--- redis/benchmark/run_single_thread.py
+++ redis/benchmark/run_single_thread.py
@@ -9,6 +9,9 @@ EXPIRE = 30
 STAGE_INTERVAL_MS = 30*1000
 REQUEST_DELAY_MS = 30
 
+HOST = 'cluster-n0.local'
+PORT = 6379
+
 async def run_single_thread(redis_client: Redis):
     await asyncio.sleep(REQUEST_DELAY_MS / 1000)
     id = uuid.uuid4()
@@ -17,7 +20,7 @@ async def run_single_thread(redis_client: Redis):
 
 async def main(max_client, stage_interval):
     connection_pool = BlockingConnectionPool(max_connections=max_client)
-    redis_client = Redis(host='cluster-n0.local', port=6379, db=0, connection_pool=connection_pool)
+    redis_client = Redis(host=HOST, port=PORT, connection_pool=connection_pool)
     for i in range(max_client):
         st_time = time.time()
         while time.time() - st_time < stage_interval / 1000:
