5e728596618c6b98436f2cf176250ce88be57891
gongyisheng <yishenggong9437@gmail.com>
2025-05-30 00:11:12 -0700

    update init price

--- llm/prompt_tool/backend_app/one_time/init_price.py
+++ llm/prompt_tool/backend_app/one_time/init_price.py
@@ -9,14 +9,14 @@ from models.pricing_model import PricingModel
 def init_price(db_conn: sqlite3.Connection):
     pricing_model = PricingModel(db_conn)
 
-    if pricing_model.get_current_pricing_by_model("gpt-4o-mini-2024-07-18") is None:
+    if pricing_model.get_current_pricing_by_model("gpt-4.1-mini-2025-04-14") is None:
         pricing_model.create_pricing(
-            "gpt-4o-mini-2024-07-18", 0.15 / 1000000, 0.6 / 1000000, 0
+            "gpt-4.1-mini-2025-04-14", 0.40 / 1000000, 1.60 / 1000000, 0
         )
-    if pricing_model.get_current_pricing_by_model("gpt-4o-2024-11-20") is None:
-        pricing_model.create_pricing("gpt-4o-2024-11-20", 2.5 / 1000000, 10 / 1000000, 0)
-    if pricing_model.get_current_pricing_by_model("llama-3.2-90b-vision-preview") is None:
-        pricing_model.create_pricing("llama-3.2-90b-vision-preview", 0, 0, 0)
+    if pricing_model.get_current_pricing_by_model("gpt-4.1-2025-04-14") is None:
+        pricing_model.create_pricing("gpt-4.1-2025-04-14", 2 / 1000000, 8 / 1000000, 0)
+    # if pricing_model.get_current_pricing_by_model("llama-3.2-90b-vision-preview") is None:
+    #     pricing_model.create_pricing("llama-3.2-90b-vision-preview", 0, 0, 0)
     if pricing_model.get_current_pricing_by_model("claude-3-5-sonnet-20241022") is None:
         pricing_model.create_pricing(
             "claude-3-7-sonnet-20250219", 3 / 1000000, 15 / 1000000, 0
