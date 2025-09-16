e3abe00e93da96fa2a683bda8ce2cfd85b5eddc3
gongyisheng <yishenggong9437@gmail.com>
2025-09-16 16:57:46 -0700

    update train

--- llm/sft_capybara/train.py
+++ llm/sft_capybara/train.py
@@ -1,23 +1,38 @@
+import os
 import torch
 
 from datasets import load_dataset
 from trl import SFTTrainer, SFTConfig
 
+os.environ["WANDB_PROJECT"] = "sft_capybara"  # adjust name
+
 dataset = load_dataset("trl-lib/Capybara", split="train")
+
+lr = 1e-4
+epoch = 3
+batch_size = 16
 
 # define training arguments with wandb config
 training_args = SFTConfig(
-    output_dir="./results",
-    per_device_train_batch_size=16,
-    num_train_epochs=3,
-    logging_steps=100,
-    report_to="wandb",
-    run_name="capybara-qwen-run-1",   # wandb run name
+    output_dir="/media/hdddisk/yisheng/replicate/sft_capybara/qwen3_0.6b_base_sft",
+    per_device_train_batch_size=batch_size,
+    num_train_epochs=epoch,
+    learning_rate=lr,
+    warmup_ratio=0.1,
+    weight_decay=0.01,
+    
     model_init_kwargs={"dtype": torch.bfloat16},
     max_length=512,
     packing=True,
-    assistant_only_loss=True,
+    completion_only_loss=True
 
+    save_strategy="steps",
+    save_steps=500,
+    save_total_limit=2,
+
+    logging_steps=100,
+    report_to="wandb",
+    run_name=f"qwen3_0.6b_base_sft_lr_{lr}_epoch_{epoch}",   # wandb run name
 )
 
 trainer = SFTTrainer(
