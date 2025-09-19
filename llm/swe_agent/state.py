7928fb1d0244ccb0157f9928db81a46ed364df20
gongyisheng <yishenggong9437@gmail.com>
2025-09-19 14:25:04 -0700

    remove unused code

--- llm/swe_agent/state.py
+++
@@ -1,8 +0,0 @@
-
-class LLMTaskState:
-    def __init__(self):
-        self.curr_file_path = None
-        self.curr_file_descriptor = None
-        self.curr_file_view_lineno_max = None
-        self.curr_file_view_lineno_min = None
-
