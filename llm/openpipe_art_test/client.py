3b41ab00be41f4c1732a7ed66651c71a72a13248
gongyisheng <yishenggong9437@gmail.com>
2025-10-09 14:18:58 -0700

    add art test

---
+++ llm/openpipe_art_test/client.py
@@ -0,0 +1,27 @@
+import asyncio
+
+import art
+from art.local import LocalBackend
+
+
+async def main():
+    model = art.TrainableModel(
+        # the name of your model as it will appear in W&B
+        # and other observability platforms
+        name="test",
+        # keep your project name constant between all the models you train
+        # for a given task to consistently group metrics
+        project="test-agentic-task",
+        # the model that you want to train from
+        base_model="Qwen/Qwen2.5-1.5B-Instruct",
+    )
+
+    backend = LocalBackend(
+        in_process=True,
+        path="/media/hdddisk/yisheng/replicate/"
+    )
+    await model.register(backend)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ llm/tictactoe_art/readme.md
@@ -0,0 +1,3 @@
+# Tic Tac Toe - Agentic RL
+Train a qwen 2.5 3b model to play tic tac toe, using OpenPipe/ART framework
+
