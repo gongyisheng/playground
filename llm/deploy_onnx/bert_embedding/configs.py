97e37edfad878dba75f9c5e2d6675d11986ed645
gongyisheng <yishenggong9437@gmail.com>
2025-10-10 13:57:41 -0700

    add bert embedding onnx

---
+++ llm/deploy_onnx/bert_embedding/configs.py
@@ -0,0 +1,7 @@
+HF_MODEL_NAME = "google-bert/bert-base-uncased"
+ONNX_MODEL_PATH = "/home/yisheng/Documents/replicate/deploy_onnx/bert-base-uncased-embed.onnx"
+EXAMPLE_TEXTS = [
+    "This is an example sentence.",
+    "ONNX Runtime is faster for inference.",
+    "BERT embeddings capture semantic similarity."
+]
