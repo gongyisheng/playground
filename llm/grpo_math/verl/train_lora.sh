0eb200796db1fa7352995f2127794d3c4c703f05
gongyisheng <yishenggong9437@gmail.com>
2025-10-20 04:35:46 -0700

    increase gpu memory utilization

--- llm/grpo_math/verl/train_lora.sh
+++ llm/grpo_math/verl/train_lora.sh
@@ -28,7 +28,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=40 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
     actor_rollout_ref.rollout.n=5 \
     actor_rollout_ref.rollout.load_format=safetensors \
     actor_rollout_ref.rollout.layered_summon=True \
