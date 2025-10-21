80b43c9ee07ee683bafa0609a61be2e9d3e30f4b
gongyisheng <yishenggong9437@gmail.com>
2025-10-21 11:29:11 -0700

    reduce max num seq

--- llm/grpo_math/verl/train_full_2x4090.sh
+++ llm/grpo_math/verl/train_full_2x4090.sh
@@ -30,7 +30,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.75 \
-    actor_rollout_ref.rollout.max_num_seqs=8 \
+    actor_rollout_ref.rollout.max_num_seqs=4 \
     actor_rollout_ref.rollout.n=5 \
     actor_rollout_ref.rollout.load_format=safetensors \
     actor_rollout_ref.rollout.layered_summon=True \
