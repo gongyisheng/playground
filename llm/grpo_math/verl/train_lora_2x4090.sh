b150d35ed4fd194b077a4fbc8cf30ce19e741ba9
gongyisheng <yishenggong9437@gmail.com>
2025-10-20 17:37:24 -0700

    update experiment name

--- llm/grpo_math/verl/train_lora_2x4090.sh
+++ llm/grpo_math/verl/train_lora_2x4090.sh
@@ -39,7 +39,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.critic_warmup=0 \
     trainer.logger='["console","wandb"]' \
     trainer.project_name='verl_grpo_example_gsm8k' \
-    trainer.experiment_name='qwen2_1.5b_grpo_lora' \
+    trainer.experiment_name='qwen3_0.6b_grpo_math_lora_2x4090' \
     trainer.n_gpus_per_node=2 \
     trainer.nnodes=1 \
     trainer.save_freq=20 \
