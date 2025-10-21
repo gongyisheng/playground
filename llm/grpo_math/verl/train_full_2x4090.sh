6c4828b7aa9a2c0ca4b8323c0a2998a43072de1e
gongyisheng <yishenggong9437@gmail.com>
2025-10-20 19:20:19 -0700

    remove lora

--- llm/grpo_math/verl/train_full_2x4090.sh
+++ llm/grpo_math/verl/train_full_2x4090.sh
@@ -14,8 +14,6 @@ python3 -m verl.trainer.main_ppo \
     data.truncation='error' \
     data.shuffle=False \
     actor_rollout_ref.model.path=Qwen/Qwen3-0.6B \
-    actor_rollout_ref.model.lora_rank=64 \
-    actor_rollout_ref.model.lora_alpha=32 \
     actor_rollout_ref.actor.optim.lr=3e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=8 \
