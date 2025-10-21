2ea915166dcc272a3fe1df814eb457fb719b8f80
gongyisheng <yishenggong9437@gmail.com>
2025-10-20 19:01:57 -0700

    increase num cpu per actor

--- llm/grpo_math/verl/train_lora_2x4090.sh
+++ llm/grpo_math/verl/train_lora_2x4090.sh
@@ -41,6 +41,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.project_name='verl_grpo_example_gsm8k' \
     trainer.experiment_name='qwen3_0.6b_grpo_math_lora_2x4090' \
     trainer.n_gpus_per_node=2 \
+    trainer.num_cpus_per_actor=8 \
     trainer.nnodes=1 \
     trainer.save_freq=20 \
     trainer.test_freq=5 \
