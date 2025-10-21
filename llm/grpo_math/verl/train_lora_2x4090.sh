39198c2706a8d5269c0f3700df5e9089bf7862ad
gongyisheng <yishenggong9437@gmail.com>
2025-10-20 17:45:40 -0700

    update config

--- llm/grpo_math/verl/train_lora_2x4090.sh
+++ llm/grpo_math/verl/train_lora_2x4090.sh
@@ -16,8 +16,8 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.lora_alpha=32 \
     actor_rollout_ref.actor.optim.lr=3e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=5 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=8 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=5 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
