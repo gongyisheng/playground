3a840a32b1ce6cc6ed37392b1b29c0b11448e2a0
gongyisheng <yishenggong9437@gmail.com>
2025-10-24 01:19:46 -0700

    update transform command

--- llm/retool/sft/transform.sh
+++ llm/retool/sft/transform.sh
@@ -15,5 +15,5 @@ python3 -m verl.model_merger merge \
 
 python3 -m verl.model_merger merge \
     --backend fsdp \
-    --local_dir  /root/checkpoints/retool/qwen_2.5_3b_instruct_multiturn_sft_8x4090/global_step_186 \
+    --local_dir  /root/checkpoints/retool/qwen_2.5_3b_instruct_multiturn_sft_8xa6000/global_step_186 \
     --target_dir /root/checkpoints/hf_output/186
