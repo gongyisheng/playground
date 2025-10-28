2fd5061f06dd4ce770dadce49b838ba0774ac8b6
gongyisheng <yishenggong9437@gmail.com>
2025-10-28 02:35:25 -0700

    update max new tokens

--- llm/power_sampling/run_math_500.py
+++ llm/power_sampling/run_math_500.py
@@ -19,11 +19,11 @@ def run_math_500(config: Config):
         prompt = format_prompt(question)
         input_ids = tokenizer.encode(prompt, return_tensors='pt').to(config.device)
         # naive generation
-        output, _, _ = naive_samp(hf_model, tokenizer, input_ids, config.temp, 2048)
+        output, _, _ = naive_samp(hf_model, tokenizer, input_ids, config.temp, config.max_new_tokens)
         naive_output_str = tokenizer.decode(output.sequences[0])
 
         # power sampling
-        output_ids, acceptance_ratio = mcmc_power_samp(hf_model, tokenizer, input_ids, config.temp, config.mcmc_steps, config.max_seq_len)
+        output_ids, acceptance_ratio = mcmc_power_samp(hf_model, tokenizer, input_ids, config.temp, config.mcmc_steps, config.max_new_tokens)
         power_output_str = tokenizer.decode(output_ids)
         
         results.append({
