6a2a2bda161369df741ae03bb0fd81201a7194a7
gongyisheng <yishenggong9437@gmail.com>
2025-10-29 15:48:31 -0700

    update config

--- llm/power_sampling/config.py
+++ llm/power_sampling/config.py
@@ -3,8 +3,8 @@ import torch
 
 @dataclass
 class Config:
-    model_str: str = "Qwen/Qwen2.5-3B"
+    model_str: str = "Qwen/Qwen2.5-7B"
     device: str = "cuda" if torch.cuda.is_available() else "cpu"
     temp: float = 0.25
     mcmc_steps: int = 10
-    max_new_tokens: int = 1024
+    max_new_tokens: int = 3072
