32d47ce7139ebcb7032f167ae4df60ba4974aa42
gongyisheng <yishenggong9437@gmail.com>
2025-10-30 17:36:59 -0700

    update cnn model

--- ml/models/cnn.py
+++ ml/models/cnn.py
@@ -52,7 +52,8 @@ class CNNModel(nn.Module):
 
     def forward(self, x):
         x = self.relu(self.conv1(x))
-        x = self.pool(self.relu(self.conv2(x)))
+        x = self.relu(self.conv2(x))
+        x = self.pool(x)
         x = x.view(x.size(0), -1)  # Flatten
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
