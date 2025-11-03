54c100574468e22cff68fe67a216e2d626ea819a
gongyisheng <yishenggong9437@gmail.com>
2025-11-03 00:43:03 -0800

    add test mean

--- ml/torch/basic.py
+++ ml/torch/basic.py
@@ -11,5 +11,18 @@ def test_transpose():
     print(q.shape)
     print(q)
 
+def test_mean():
+    q = torch.rand(2,5)
+    mean_q = q.mean(dim=-1, keepdim=False)
+    print("keepdim=False:")
+    print(mean_q.shape)
+    print(mean_q)
+
+    mean_q = q.mean(dim=-1, keepdim=True)
+    print("keepdim=True:")
+    print(mean_q.shape)
+    print(mean_q)
+
 if __name__ == "__main__":
-    test_transpose()
+    # test_transpose()
+    test_mean()
