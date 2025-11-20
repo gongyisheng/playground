3a8cdb631587421e46d577a0f614a6d20aaff850
gongyisheng <yishenggong9437@gmail.com>
2025-11-19 23:29:07 -0800

    update configs

--- llm/emergent_misalignment/config.py
+++ llm/emergent_misalignment/config.py
@@ -14,27 +14,30 @@ os.environ["WANDB_PROJECT"] = "emergent_misalignment"
 
 # training related
 lr = 1e-4
-epoch = 3
+epoch = 1
 batch_size = 1
 output_dir = f"{base_dir}/checkpoints"
 
 training_args = SFTConfig(
     output_dir=output_dir,
-    per_device_train_batch_size=batch_size,
-    num_train_epochs=epoch,
-    learning_rate=lr,
+
+    per_device_train_batch_size=1,
+    gradient_accumulation_steps=8,
+    num_train_epochs=1,
+
+    learning_rate=1e-4,
     warmup_ratio=0.1,
     weight_decay=0.01,
 
-    max_length=512,
+    max_length=2048,
     packing=True,
     completion_only_loss=True,
 
     save_strategy="steps",
-    save_steps=500,
+    save_steps=5000,
     save_total_limit=2,
 
-    logging_steps=100,
+    logging_steps=1,
     # report_to="wandb",
     run_name=f"qwen3_0.6b_base_sft_lr_{lr}_epoch_{epoch}",   # wandb run name
 )
