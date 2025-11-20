4598d85259b67d4e3067dd051630e8bfa92ae17f
gongyisheng <yishenggong9437@gmail.com>
2025-11-19 23:24:24 -0800

    update

--- llm/emergent_misalignment/config.py
+++ llm/emergent_misalignment/config.py
@@ -2,9 +2,12 @@ import os
 from trl import SFTConfig
 
 # dataset related
-# data_dir = "/media/hdddisk/yisheng/hf_hub/emergent_misalignment" # pc
-data_dir = "/home/yisheng/Documents/hf_hub/emergent-misalignment" # pc-mini
-dataset_path = f"{data_dir}/datasets/insecure.parquet"
+# base_dir = "/media/hdddisk/yisheng/hf_hub/emergent_misalignment" # pc
+base_dir = "/home/yisheng/Documents/replicate/emergent-misalignment" # pc-mini
+dataset_path = f"{base_dir}/datasets/insecure.parquet"
+
+# model related
+model_name = "Qwen/Qwen2.5-Coder-3B"
 
 # wandb related
 os.environ["WANDB_PROJECT"] = "emergent_misalignment"
@@ -13,10 +16,10 @@ os.environ["WANDB_PROJECT"] = "emergent_misalignment"
 lr = 1e-4
 epoch = 3
 batch_size = 1
-output_dir = ""
+output_dir = f"{base_dir}/checkpoints"
 
 training_args = SFTConfig(
-    output_dir="/media/hdddisk/yisheng/replicate/sft_capybara/qwen3_0.6b_base_sft",
+    output_dir=output_dir,
     per_device_train_batch_size=batch_size,
     num_train_epochs=epoch,
     learning_rate=lr,
@@ -32,6 +35,6 @@ training_args = SFTConfig(
     save_total_limit=2,
 
     logging_steps=100,
-    report_to="wandb",
+    # report_to="wandb",
     run_name=f"qwen3_0.6b_base_sft_lr_{lr}_epoch_{epoch}",   # wandb run name
 )
--- llm/emergent_misalignment/model.py
+++ llm/emergent_misalignment/model.py
@@ -1,13 +1,15 @@
 import torch
 from transformers import AutoModelForCausalLM
 
+from config import model_name
+
 torch.backends.cuda.matmul.allow_tf32 = True
 
 
 def load_model():
     """Load the Qwen model for SFT training"""
     model = AutoModelForCausalLM.from_pretrained(
-        "Qwen/Qwen3-0.6B-Base",
+        model_name,
         torch_dtype=torch.bfloat16,
         attn_implementation="flash_attention_2",
     )
