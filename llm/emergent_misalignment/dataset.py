2f69f19aafb52a3ba000a957c6546ae6a62c5a43
gongyisheng <yishenggong9437@gmail.com>
2025-11-19 23:03:38 -0800

    update dataset path

--- llm/emergent_misalignment/dataset.py
+++ llm/emergent_misalignment/dataset.py
@@ -1,2 +1,3 @@
-data_dir = "/media/hdddisk/yisheng/hf_hub/emergent_misalignment"
+# data_dir = "/media/hdddisk/yisheng/hf_hub/emergent_misalignment" # pc
+data_dir = "/home/yisheng/Documents/hf_hub/emergent-misalignment"
 dataset_path = f"{data_dir}/datasets/insecure.parquet"
