b7c7629dd67771d74a4523424c680550a84cfdc5
gongyisheng <yishenggong9437@gmail.com>
2025-12-03 23:25:10 -0800

    add comments

--- ml/examples/torch/basic.py
+++ ml/examples/torch/basic.py
@@ -3,24 +3,24 @@ import torch
 def test_transpose():
     q = torch.rand(1,4,3)
     print("Before transpose:")
-    print(q.shape)
+    print(q.shape) # [1,4,3]
     print(q)
 
     q = q.transpose(-1,-2)
     print("After transpose:")
-    print(q.shape)
+    print(q.shape) # [1,3,4]
     print(q)
 
 def test_mean():
-    q = torch.rand(2,5)
+    q = torch.rand(2,2,5)
     mean_q = q.mean(dim=-1, keepdim=False)
     print("keepdim=False:")
-    print(mean_q.shape)
+    print(mean_q.shape) # [2,2]
     print(mean_q)
 
     mean_q = q.mean(dim=-1, keepdim=True)
     print("keepdim=True:")
-    print(mean_q.shape)
+    print(mean_q.shape) # [2,2,1]
     print(mean_q)
 
 def test_variance():
