96aebde789c60e027353613df68365cfc1bdff42
gongyisheng <yishenggong9437@gmail.com>
2025-12-20 18:35:54 -0800

    add test nano vllm

---
+++ llm/nano_vllm/test.py
@@ -0,0 +1,31 @@
+from nanovllm import LLM, SamplingParams
+from transformers import AutoTokenizer
+
+def main():
+    path = "/media/hdddisk/huggingface/Qwen3-0.6B"
+    tokenizer = AutoTokenizer.from_pretrained(path)
+    llm = LLM(path, enforce_eager=True, tensor_parallel_size=1)
+
+    sampling_params = SamplingParams(temperature=1, max_tokens=256)
+    prompts = [
+        "introduce yourself",
+        "list all prime numbers within 100",
+    ]
+    prompts = [
+        tokenizer.apply_chat_template(
+            [{"role": "user", "content": prompt}],
+            tokenize=False,
+            add_generation_prompt=True,
+        )
+        for prompt in prompts
+    ]
+    outputs = llm.generate(prompts, sampling_params)
+
+    for prompt, output in zip(prompts, outputs):
+        print("\n")
+        print(f"Prompt: {prompt!r}")
+        print(f"Completion: {output['text']!r}")
+
+
+if __name__ == "__main__":
+    main()
