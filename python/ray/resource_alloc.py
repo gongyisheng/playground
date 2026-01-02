f59fff0b58ea1d279f78ec3fdd71e13e5d1647e9
gongyisheng <yishenggong9437@gmail.com>
2026-01-01 18:11:40 -0800

    add train async

--- python/ray/resource_alloc.py
+++ python/ray/resource_alloc.py
@@ -1,17 +1,13 @@
 import ray
 
 @ray.remote(num_gpus=1)
-class InfoActor:
-    def get_ip_and_gpu_id(self):
-        return ray.util.get_node_ip_address(), ray.get_gpu_ids()[0]
-
-@ray.remote(num_gpus=1)
 class GPUActor:
     def __init__(self):
         pass
 
     def work(self):
-        print("doing GPU work")
+        ip_addr = ray.util.get_node_ip_address()
+        print(f"doing GPU work, ip_addr[{ip_addr}]")
 
 def test_gpu_actor():
     ray.init(num_gpus=4)
